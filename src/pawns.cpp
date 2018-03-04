28210b4cdee2801be91b9bf21f180f5fc8691eb1
protonspring <mike@whiteley.org>
2018-03-04 13:40:50 -0700

    remove more unnecessary logic.

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -156,7 +156,9 @@ namespace {
         else if (!neighbours)
             score -= Isolated, e->weakUnopposed[Us] += !opposed;
 
-        else if (neighbours && !lever && relative_rank(Us, s) < RANK_5)
+        // A pawn is backward when it is behind all pawns of the same color on the
+        // adjacent files and cannot be safely advanced.
+        else if (!lever && relative_rank(Us, s) < RANK_5)
         {
             // Find the backmost rank with neighbours or stoppers
             b = rank_bb(backmost_sq(Us, neighbours | stoppers));
