d69081ed99d523083c37e1bc78a8b672b64eb27a
protonspring <mike@whiteley.org>
2018-03-22 20:01:26 -0600

    S(20,20)

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -38,7 +38,7 @@ namespace {
   constexpr Score Backward = S(24, 12);
 
   // Bonus for passed pawns that are neighbors
-  constexpr Score PassedNeighbours = S(4,4);
+  constexpr Score PassedNeighbours = S(20,20);
 
   // Connected pawn bonus by opposed, phalanx, #support and rank
   Score Connected[2][2][3][RANK_NB];
