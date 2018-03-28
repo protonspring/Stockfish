04206a0a7d860958d85878343d7cd9e5610eef03
protonspring <mike@whiteley.org>
2018-03-28 08:12:07 -0600

    tmp

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -47,10 +47,14 @@ namespace {
   // RANK_1 = 0 is for files where we have no pawns or our pawn is behind our king.
   Value NoPawnOnKingFile = V(12);
   constexpr Value ShelterStrength[4][4] = {
-      { V( 12), V( 93), V( 98), V(68) },
-      { V(  7), V(101), V( 75), V(24) },
-      { V( 10), V(106), V( 47), V(15) },
-      { V( 37), V(103), V( 59), V(37) }
+      { V(  0), V( 88), V( 88), V(59) },
+      { V(  0), V(107), V( 68), V(37) },
+      { V(  0), V(109), V( 58), V(20) },
+      { V(  0), V( 98), V( 51), V( 0) }
+      //{ V( 12), V( 93), V( 98), V(68) },
+      //{ V(  7), V(101), V( 75), V(24) },
+      //{ V( 10), V(106), V( 47), V(15) },
+      //{ V( 37), V(103), V( 59), V(37) }
   };
 
   // Danger of enemy pawns moving toward our king by [type][distance from edge][rank].
