bcc26652aae8c513bc9b72a749b59fdfbd13985d
protonspring <mike@whiteley.org>
2018-04-08 16:57:15 -0600

    formatting.

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -130,7 +130,7 @@ namespace {
         neighbours = ourPawns   & adjacent_files_bb(f);
         phalanx    = neighbours & rank_bb(s);
         supported  = neighbours & rank_bb(s - Up);
-        
+
         // A pawn is backward when it is behind all pawns of the same color on the
         // adjacent files and cannot be safely advanced.
         if (!neighbours || lever || relative_rank(Us, s) >= RANK_5)
