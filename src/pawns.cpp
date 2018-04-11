aea03b84761c6b17c56a7812c07a7c06aa4f5405
protonspring <mike@whiteley.org>
2018-04-11 15:55:19 -0600

    one square ahead of the most advanced enemy pawn.

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -275,6 +275,7 @@ Value Entry::shelter_storm(const Position& pos, Square ksq) {
 template<Color Us>
 Score Entry::do_king_safety(const Position& pos, Square ksq) {
 
+  const Direction Down = Us == WHITE ? SOUTH : NORTH;
   kingSquares[Us] = ksq;
   castlingRights[Us] = pos.can_castle(Us);
   int minKingPawnDistance = 0;
@@ -283,7 +284,7 @@ Score Entry::do_king_safety(const Position& pos, Square ksq) {
   if (pawns)
       while (!(DistanceRingBB[ksq][minKingPawnDistance++] & pawns)) {}
   else
-     minKingPawnDistance = distance(ksq,backmost_sq(Us,pos.pieces(PAWN)));
+     minKingPawnDistance = distance(ksq,backmost_sq(Us,shift<Down>(pos.pieces(PAWN))));
 
   Value bonus = shelter_storm<Us>(pos, ksq);
 
