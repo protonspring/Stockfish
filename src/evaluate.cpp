fa1c685d65cdbe4d6289797721335bdde68dbf1e
protonspring <mike@whiteley.org>
2018-04-13 09:23:47 -0600

    tiny bonus * distance for being close to the enemy king.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -181,6 +181,7 @@ namespace {
   constexpr Score TrappedRook        = S( 92,  0);
   constexpr Score WeakQueen          = S( 50, 10);
   constexpr Score WeakUnopposedPawn  = S(  5, 25);
+  constexpr Score CloseToEnemyKing   = S(  1,  1);
 
 #undef S
 
@@ -300,6 +301,7 @@ namespace {
 
     Bitboard b, bb;
     Square s;
+    Square eksq = pos.square<KING>(Them);
     Score score = SCORE_ZERO;
     int mob;
 
@@ -307,6 +309,9 @@ namespace {
 
     while ((s = *pl++) != SQ_NONE)
     {
+        // a tiny bonus for being close to the enemy king
+        score += CloseToEnemyKing * distance(eksq,s);
+
         // Find attacked squares, including x-ray attacks for bishops and rooks
         b = Pt == BISHOP ? attacks_bb<BISHOP>(s, pos.pieces() ^ pos.pieces(QUEEN))
           : Pt ==   ROOK ? attacks_bb<  ROOK>(s, pos.pieces() ^ pos.pieces(QUEEN) ^ pos.pieces(Us, ROOK))
