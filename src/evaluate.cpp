c1e1fbdaa4bcc6f9cccb9382cfee6846f45824c1
protonspring <mike@whiteley.org>
2018-04-13 12:12:03 -0600

    updated values.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -181,7 +181,7 @@ namespace {
   constexpr Score TrappedRook        = S( 92,  0);
   constexpr Score WeakQueen          = S( 50, 10);
   constexpr Score WeakUnopposedPawn  = S(  5, 25);
-  constexpr Score EnemyKingDistance  = S( -1,  3);
+  constexpr Score EnemyKingDistance  = S( -1,  2);
 
 #undef S
 
