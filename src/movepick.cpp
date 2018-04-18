5ada3a0bcf616908b3232d66a78069e35907ff5f
protonspring <mike@whiteley.org>
2018-04-17 22:44:47 -0600

    reduced histories. 1 and 3.

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -115,7 +115,6 @@ void MovePicker::score() {
 
       else if (Type == QUIETS)
           m.value =  (*mainHistory)[pos.side_to_move()][from_to(m)]
-                   + (*contHistory[0])[pos.moved_piece(m)][to_sq(m)]
                    + (*contHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    + (*contHistory[3])[pos.moved_piece(m)][to_sq(m)];
 
