9b1064721c4450e6513bb1c8b9acdf3c46afe83c
protonspring <mike@whiteley.org>
2018-04-22 07:23:07 -0600

    updated values from tuning #1.

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -45,15 +45,15 @@ namespace {
 
   // Weakness of our pawn shelter in front of the king by [distance from edge][rank].
   // RANK_1 = 0 is used for files where we have no pawns or our pawn is behind our king.
-  constexpr Value BaseSafety = Value(-73);
-  constexpr Value NoPawnOnKingFile[FILE_NB] = 
-    { V(10), V(10), V(10), V(10), V(10), V(10), V(10), V(10) };
+  Value BaseSafety = Value(-67);
+  Value NoPawnOnKingFile[FILE_NB] = 
+    { V( 8), V(11), V( 9), V(12), V(11), V( 5), V(11), V(14) };
 
-  constexpr Value ShelterStrength[int(FILE_NB) / 2][RANK_NB] = {
-    { V( 13), V( 84), V( 99), V( 68), V( 26), V( 24), V(  9) },
-    { V(  7), V( 96), V( 77), V( 25), V( 19), V(  5), V(  0) },
-    { V( 10), V(110), V( 49), V( 13), V( 51), V( 18), V( -8) },
-    { V( 37), V(104), V( 54), V( 39), V( 27), V( 25), V( -2) }
+  Value ShelterStrength[int(FILE_NB) / 2][RANK_NB] = {
+    { V( 15), V( 88), V(101), V( 68), V( 28), V( 23), V( 13) },
+    { V(  9), V(107), V( 75), V( 14), V( 25), V( 14), V( -4) },
+    { V(  6), V(108), V( 51), V( 12), V( 45), V( 17), V(-12) },
+    { V( 36), V(103), V( 49), V( 43), V( 33), V( 18), V( -9) }
   };
 
   // Danger of enemy pawns moving toward our king by [type][distance from edge][rank].
