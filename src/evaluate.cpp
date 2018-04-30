6c3c555beb4cb79aba02d13aceae4991ae30e8f4
protonspring <mike@whiteley.org>
2018-04-29 22:27:26 -0600

    S(5,10)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -181,7 +181,7 @@ namespace {
   constexpr Score TrappedRook        = S( 92,  0);
   constexpr Score WeakQueen          = S( 50, 10);
   constexpr Score WeakUnopposedPawn  = S(  5, 25);
-  constexpr Score KnightWidePawns    = S(  0, 20);
+  constexpr Score KnightWidePawns    = S(  5, 10);
 
 #undef S
 
