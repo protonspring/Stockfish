80a8c971d6e00dc21141288348189a572b1a0a7e
protonspring <mike@whiteley.org>
2018-05-07 08:35:07 -0600

    remove a condition.
    bench 5256021

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -131,8 +131,7 @@ namespace {
             && popcount(phalanx)   >= popcount(leverPush))
             e->passedPawns[Us] |= s;
 
-        else if (   stoppers == SquareBB[s + Up]
-                 && relative_rank(Us, s) >= RANK_5)
+        else if (stoppers == SquareBB[s + Up])
         {
             b = shift<Up>(supported) & ~theirPawns;
             while (b)
