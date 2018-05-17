d1693b987585a872595e22ddb8d6e710e7796ac9
protonspring <mike@whiteley.org>
2018-05-17 15:21:48 -0600

    -4

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -54,7 +54,7 @@ namespace {
 
   // Danger of unblocked enemy pawns moving toward our king by rank.
   // RANK_1 is used when there is no enemy pawn on the file
-  constexpr Value Unblocked[RANK_NB] = {V(25), V(67), V(130), V(66), V(39)};
+  constexpr Value Unblocked[RANK_NB] = {V(15), V(57), V(120), V(56), V(29)};
 
   // Danger of blocked enemy pawns ahead of king by [distance from edge][rank]
   constexpr Value BlockedByPawn[FILE_NB / 2][RANK_NB] =
