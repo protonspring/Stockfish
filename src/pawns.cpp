84dd663e2d422c1d92dfdcbaa74bec8b451bdf90
protonspring <mike@whiteley.org>
2018-05-21 08:31:58 -0600

    118 for rank_3.

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -56,10 +56,10 @@ namespace {
   // For the unblocked case, RANK_1 = 0 is used when opponent has no pawn on the
   // given file, or their pawn is behind our king.
   constexpr Value StormDanger[][4][RANK_NB] = {
-    { { V(25),  V( 79), V(107), V( 51), V( 26) },  // UnBlocked
-      { V(15),  V( 45), V(131), V( 51), V( 26) },
+    { { V(25),  V( 79), V(118), V( 51), V( 26) },  // UnBlocked
+      { V(15),  V( 45), V(118), V( 51), V( 26) },
       { V( 0),  V( 42), V(118), V( 51), V( 26) },
-      { V( 3),  V( 54), V(110), V( 51), V( 26) } },
+      { V( 3),  V( 54), V(118), V( 51), V( 26) } },
     { { V( 0),  V(  0), V( 37), V(  5), V(-48) },  // BlockedByPawn
       { V( 0),  V(  0), V( 68), V(-12), V( 13) },
       { V( 0),  V(  0), V(111), V(-25), V( -3) },
