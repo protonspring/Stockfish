974db1fbccba576de80a09289f4a76b423b8eb04
protonspring <mike@whiteley.org>
2018-05-22 14:51:25 -0600

    remove rank7.

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -46,23 +46,23 @@ namespace {
   // Strength of pawn shelter for our king by [distance from edge][rank].
   // RANK_1 = we have no pawn on the file or our pawn is behind our king.
   constexpr Value ShelterStrength[int(FILE_NB) / 2][RANK_NB] = {
-    { V(  7), V(76), V( 84), V( 38), V(  7), V( 30), V(-19) },
-    { V(-13), V(83), V( 42), V(-27), V(  2), V(-32), V(-45) },
-    { V(-26), V(63), V(  5), V(-44), V( -5), V(  2), V(-59) },
-    { V(-19), V(53), V(-11), V(-22), V(-12), V(-51), V(-60) }
+    { V(  7), V(76), V( 84), V( 38), V(  7), V( 30) },
+    { V(-13), V(83), V( 42), V(-27), V(  2), V(-32) },
+    { V(-26), V(63), V(  5), V(-44), V( -5), V(  2) },
+    { V(-19), V(53), V(-11), V(-22), V(-12), V(-51) }
   };
 
   // Danger of enemy pawns moving toward our king by [distance from edge][rank].
   // RANK_1 = the opponent has no pawn on the file or the pawn is behind our king
   constexpr Value UnBlocked[4][RANK_NB] =
-    { { V( 25), V( 79), V(107), V( 51), V( 27), V(  0), V(  0) },
-      { V(  5), V( 35), V(121), V( -2), V( 15), V(-10), V(-10) },
-      { V(-20), V( 22), V( 98), V( 36), V(  7), V(-20), V(-20) },
-      { V(-27), V( 24), V( 80), V( 25), V( -4), V(-30), V(-30) } };
+    { { V( 25), V( 79), V(107), V( 51), V( 27), V(  0) },
+      { V(  5), V( 35), V(121), V( -2), V( 15), V(-10) },
+      { V(-20), V( 22), V( 98), V( 36), V(  7), V(-20) },
+      { V(-27), V( 24), V( 80), V( 25), V( -4), V(-30) } };
 
   // Danger of blocked pawns ahead of our king by rank.
   constexpr Value BlockedByPawn[RANK_NB] =
-      { V(  0), V(  0), V( 75), V(-10), V(-20), V(-20), V(-20) };
+      { V(  0), V(  0), V( 75), V(-10), V(-20), V(-20) };
 
   #undef S
   #undef V
