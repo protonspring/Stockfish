fcee17063009d7ad850b128d910e3aae95dde4d4
protonspring <mike@whiteley.org>
2018-05-27 19:33:43 -0600

    shelter, rank1, base 90

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -46,10 +46,10 @@ namespace {
   // Strength of pawn shelter for our king by [distance from edge][rank].
   // RANK_1 = 0 is used for files where we have no pawn, or pawn is behind our king.
   constexpr Value ShelterStrength[int(FILE_NB) / 2][RANK_NB] = {
-    { V(  7), V(78), V( 84), V( 38), V(  7), V( 30), V(-19) },
-    { V(-13), V(68), V( 42), V(-27), V(  2), V(-32), V(-45) },
-    { V(-26), V(58), V(  5), V(-44), V( -5), V(  2), V(-59) },
-    { V(-19), V(48), V(-11), V(-22), V(-12), V(-51), V(-60) }
+    { V(  7), V(90), V( 84), V( 38), V(  7), V( 30), V(-19) },
+    { V(-13), V(80), V( 42), V(-27), V(  2), V(-32), V(-45) },
+    { V(-26), V(70), V(  5), V(-44), V( -5), V(  2), V(-59) },
+    { V(-19), V(60), V(-11), V(-22), V(-12), V(-51), V(-60) }
   };
 
   // Danger of enemy pawns moving toward our king by [distance from edge][rank].
