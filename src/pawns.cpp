bfb0c1c6eba3045ec8b9d522ffde5cb487382746
protonspring <mike@whiteley.org>
2018-06-09 13:44:11 -0600

    first rule for weak pawns.

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -34,7 +34,8 @@ namespace {
   // Pawn penalties
   constexpr Score Isolated = S(13, 16);
   constexpr Score Backward = S(17, 11);
-  constexpr Score Doubled = S(13, 40);
+  constexpr Score Doubled  = S(13, 40);
+  constexpr Score Alone    = S( 5,  5);
 
   // Connected pawn bonus by opposed, phalanx, #support and rank
   Score Connected[2][2][3][RANK_NB];
@@ -144,6 +145,9 @@ namespace {
 
         if (doubled && !supported)
             score -= Doubled;
+
+        if (!phalanx && !supported)
+            score -= Alone;
     }
 
     return score;
