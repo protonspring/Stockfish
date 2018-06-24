89c2ccee9a96bcaaafccf733e7bbb703cb443407
protonspring <mike@whiteley.org>
2018-06-23 21:15:03 -0600

    remove all but rank3

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -60,7 +60,7 @@ namespace {
 
   // Danger of blocked enemy pawns storming our king, by rank
   constexpr Value BlockedStorm[RANK_NB] =
-    { V(0), V(0), V( 81), V(-9), V(-5), V(-1), V(26) };
+    { V(0), V(0), V( 81), V( 0), V( 0), V( 0), V( 0) };
 
   #undef S
   #undef V
