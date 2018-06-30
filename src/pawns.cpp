e2a7393739153ac65d75e71f72c7ad875331a1fe
protonspring <mike@whiteley.org>
2018-06-30 00:36:32 -0600

    another try.

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -124,6 +124,7 @@ namespace {
             e->passedPawns[Us] |= s;
 
         else if (   stoppers == SquareBB[s + Up]
+                 && relative_rank(Us,s) >= RANK_5
                  && !(ourPawns & forward_file_bb(Us, s)))
         {
             b = shift<Up>(supported) & ~theirPawns;
