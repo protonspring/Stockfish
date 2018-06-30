3e6c71e48fefa9dd83e514ff40405ba1582a057e
protonspring <mike@whiteley.org>
2018-06-29 21:10:15 -0600

    make passed pawns faster.

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -117,19 +117,25 @@ namespace {
         // full attack info to evaluate them. Include also not passed pawns
         // which could become passed after one or two pawn pushes when are
         // not attacked more times than defended.
-        if (   !(stoppers ^ lever ^ leverPush)
-            && !(ourPawns & forward_file_bb(Us, s))
-            && popcount(supported) >= popcount(lever) - 1
-            && popcount(phalanx)   >= popcount(leverPush))
-            e->passedPawns[Us] |= s;
-
-        else if (   stoppers == SquareBB[s + Up]
-                 && relative_rank(Us, s) >= RANK_5)
+        if (!(ourPawns & forward_file_bb(Us, s)))
         {
-            b = shift<Up>(supported) & ~theirPawns;
-            while (b)
-                if (!more_than_one(theirPawns & PawnAttacks[Us][pop_lsb(&b)]))
-                    e->passedPawns[Us] |= s;
+           if (!stoppers)
+               e->passedPawns[Us] |= s;
+           else if ((!(stoppers ^ lever) && //stoppers are only levers
+                    (popcount(supported) >= popcount(lever -1))))
+               e->passedPawns[Us] |= s;
+           else if (   !(stoppers ^ lever ^ leverPush)
+               && popcount(supported) >= popcount(lever) - 1
+               && popcount(phalanx)   >= popcount(leverPush))
+               e->passedPawns[Us] |= s;
+           else if (   stoppers == SquareBB[s + Up] // one stopper opposed
+                    && relative_rank(Us, s) >= RANK_5)
+           {
+               b = shift<Up>(supported) & ~theirPawns;
+               while (b)
+                   if (!more_than_one(theirPawns & PawnAttacks[Us][pop_lsb(&b)]))
+                       e->passedPawns[Us] |= s;
+           }
         }
 
         // Score this pawn
