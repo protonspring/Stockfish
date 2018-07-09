3f5191527f06593c0c231d69374806a1b792307f
protonspring <mike@whiteley.org>
2018-07-09 12:53:49 -0600

    s(10,0) non-pawn.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -178,7 +178,7 @@ namespace {
   constexpr Score TrappedRook        = S( 92,  0);
   constexpr Score WeakQueen          = S( 50, 10);
   constexpr Score WeakUnopposedPawn  = S(  5, 26);
-  constexpr Score Forking            = S(  0, 10);
+  constexpr Score Forking            = S( 10,  0);
 
 #undef S
 
