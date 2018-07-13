e19b469602b8a8af4ed82ca826814bf02d9f22ea
protonspring <mike@whiteley.org>
2018-07-13 17:45:50 -0600

    updated value to 85.

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -35,7 +35,7 @@ namespace {
   constexpr Score Isolated = S( 4, 20);
   constexpr Score Backward = S(21, 22);
   constexpr Score Doubled  = S(12, 54);
-  constexpr Value Thorned  = V(100);
+  constexpr Value Thorned  = V(85);
 
   // Connected pawn bonus by opposed, phalanx, #support and rank
   Score Connected[2][2][3][RANK_NB];
