a6cdc85200af79efc91feff6866f7c4260e31a6f
protonspring <mike@whiteley.org>
2018-07-13 20:02:19 -0600

    updated values.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -166,8 +166,8 @@ namespace {
   constexpr Score HinderPassedPawn   = S(  5, -1);
   constexpr Score KnightOnQueen      = S( 21, 11);
   constexpr Score LongDiagonalBishop = S( 22,  0);
-  constexpr Score MinorBehindPawn    = S( 16,  0);
-  constexpr Score MinorBlocksPawn    = S( 16,  0);
+  constexpr Score MinorBehindPawn    = S( 17,  1);
+  constexpr Score MinorBlocksPawn    = S( 18,  3);
   constexpr Score Overload           = S( 10,  5);
   constexpr Score PawnlessFlank      = S( 20, 80);
   constexpr Score RookOnPawn         = S(  8, 24);
