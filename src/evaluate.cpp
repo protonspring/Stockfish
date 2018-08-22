ba14fa049bc9c34b2cd2161e67503541bb4aee05
protonspring <mike@whiteley.org>
2018-08-21 18:07:30 -0600

    full bonus.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -382,7 +382,7 @@ namespace {
 
             // Bonus for rook on an almost open file (levered pawn)
             else if (pe->almostopen_file(Us, file_of(s)))
-                score += RookOnFile[bool(pe->semiopen_file(Them, file_of(s)))] / 4;
+                score += RookOnFile[bool(pe->semiopen_file(Them, file_of(s)))];
 
             // Penalty when trapped by the king, even more if the king cannot castle
             else if (mob <= 3)
