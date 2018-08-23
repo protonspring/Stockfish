9f0a823994041ef65e51f0f4aa218d8e907d50ad
protonspring <mike@whiteley.org>
2018-08-23 13:52:30 -0600

    just isolated.

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -113,7 +113,7 @@ namespace {
         backward =  !(ourPawns & pawn_attack_span(Them, s + Up))
                   && (stoppers & (leverPush | (s + Up)));
 
-        if (!(backward || !neighbours))
+        if (neighbours)
            e->semiopenFiles[Us]   &= ~(1 << f);
 
         // Passed pawns will be properly scored in evaluation because we need
