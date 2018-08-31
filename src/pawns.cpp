b7882e0978b04b8053bd81717f626b016a7ec002
protonspring <mike@whiteley.org>
2018-08-31 09:26:03 -0600

    simplify pawn shelter.

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -207,9 +207,8 @@ Value Entry::evaluate_shelter(const Position& pos, Square ksq) {
   constexpr Direction Down = (Us == WHITE ? SOUTH : NORTH);
   constexpr Bitboard  BlockRanks = (Us == WHITE ? Rank1BB | Rank2BB : Rank8BB | Rank7BB);
 
-  Bitboard b = pos.pieces(PAWN) & ~forward_ranks_bb(Them, ksq);
-  Bitboard ourPawns = b & pos.pieces(Us);
-  Bitboard theirPawns = b & pos.pieces(Them);
+  Bitboard ourPawns   = pos.pieces(  Us, PAWN);
+  Bitboard theirPawns = pos.pieces(Them, PAWN);
 
   Value safety = (shift<Down>(theirPawns) & (FileABB | FileHBB) & BlockRanks & ksq) ?
                  Value(374) : Value(5);
