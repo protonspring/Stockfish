77c19c6d4db633b52babbbe37ae0133bb58e0276
protonspring <mike@whiteley.org>
2018-09-04 16:09:49 -0600

    -55

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -212,7 +212,7 @@ Value Entry::evaluate_shelter(const Position& pos, Square ksq) {
   Bitboard theirPawns = b & pos.pieces(Them);
 
   Value safety = (shift<Down>(theirPawns) & (FileABB | FileHBB) & BlockRanks & ksq) ?
-                 Value(374-75) : Value(5-75);
+                 Value(374-55) : Value(5-55);
 
   File center = std::max(FILE_B, std::min(FILE_G, file_of(ksq)));
   for (File f = File(center - 1); f <= File(center + 1); ++f)
