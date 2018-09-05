97360c5c8f975d3f64ed0e95bf4c14729f6be10f
protonspring <mike@whiteley.org>
2018-09-05 14:57:39 -0600

    simplify scale factor.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -792,9 +792,8 @@ namespace {
     // If scale is not already specific, scale down the endgame via general heuristics
     if (sf == SCALE_FACTOR_NORMAL)
     {
-        if (   pos.opposite_bishops()
-            && pos.non_pawn_material(WHITE) == BishopValueMg
-            && pos.non_pawn_material(BLACK) == BishopValueMg)
+        if (   pos.opposite_bishops() &&
+               pos.non_pawn_material() == 2*BishopValueMg)
             sf = 31;
         else
             sf = std::min(40 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide), sf);
