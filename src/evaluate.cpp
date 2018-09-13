18b3a2ea192464d5a0dee6b2c0514c598e286e7c
protonspring <mike@whiteley.org>
2018-09-13 09:57:01 -0600

    simplify passed pawns.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -693,9 +693,8 @@ namespace {
         } // w != 0
 
         // Scale down bonus for candidate passers which need more than one
-        // pawn push to become passed, or have a pawn in front of them.
-        if (   !pos.pawn_passed(Us, s + Up)
-            || (pos.pieces(PAWN) & forward_file_bb(Us, s)))
+        // pawn push to become passed
+        if (!pos.pawn_passed(Us, s + Up))
             bonus = bonus / 2;
 
         score += bonus + PassedFile[file_of(s)];
