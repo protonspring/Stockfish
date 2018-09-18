801f6a63780968008c192f7f79023303728879a2
protonspring <mike@whiteley.org>
2018-09-18 16:33:37 -0600

    remove a condition.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -668,8 +668,7 @@ namespace {
 
                 bb = forward_file_bb(Them, s) & pos.pieces(ROOK, QUEEN) & pos.attacks_from<ROOK>(s);
 
-                if (!(pos.pieces(Us) & bb))
-                    defendedSquares &= attackedBy[Us][ALL_PIECES];
+                defendedSquares &= attackedBy[Us][ALL_PIECES];
 
                 if (!(pos.pieces(Them) & bb))
                     unsafeSquares &= attackedBy[Them][ALL_PIECES] | pos.pieces(Them);
