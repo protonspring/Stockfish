b1d8e478eadd9f244cfee809cd0d36593a9c4241
protonspring <mike@whiteley.org>
2018-09-20 17:56:11 -0600

    simplify.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -737,9 +737,7 @@ namespace {
     behind |= (Us == WHITE ? behind >> 16 : behind << 16);
 
     int bonus = popcount(safe) + popcount(behind & safe);
-    int weight = pos.count<ALL_PIECES>(Us) - 2 * pe->open_files();
-
-    Score score = make_score(bonus * weight * weight / 16, 0);
+    Score score = make_score(bonus * 16, 0);
 
     if (T)
         Trace::add(SPACE, Us, score);
