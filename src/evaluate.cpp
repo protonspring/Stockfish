6fd42256eb4d6918aa4e8a127bbdd097602fdd51
protonspring <mike@whiteley.org>
2018-09-20 14:42:34 -0600

    also consider attacks on blocking piece.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -659,7 +659,7 @@ namespace {
                 bonus -= make_score(0, king_proximity(Us, blockSq + Up) * w);
 
             // If the pawn is free to advance, then increase the bonus
-            if (pos.empty(blockSq))
+            if (pos.empty(blockSq) || (pos.pieces(Them) & attackedBy2[Us] & blockSq))
             {
                 // If there is a rook or queen attacking/defending the pawn from behind,
                 // consider all the squaresToQueen. Otherwise consider only the squares
