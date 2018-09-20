67519ea224088dd90498fdbe4e5646212c5093f6
protonspring <mike@whiteley.org>
2018-09-20 07:52:49 -0600

    8

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -681,7 +681,7 @@ namespace {
                 // If the path to the queen is fully defended, assign a big bonus.
                 // Otherwise assign a smaller bonus if the block square is defended.
                 if (defendedSquares & blockSq)
-                    k += 7;
+                    k += 8;
 
                 bonus += make_score(k * w, k * w);
             }
