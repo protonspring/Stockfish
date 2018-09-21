c9b61db5c3c78dcf841ac37b1a4342cb55b88574
protonspring <mike@whiteley.org>
2018-09-21 08:34:47 -0600

    simplify

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -762,9 +762,7 @@ namespace {
                             && (pos.pieces(PAWN) & KingSide);
 
     // Compute the initiative bonus for the attacking side
-    int complexity =   8 * pe->pawn_asymmetry()
-                    + 12 * pos.count<PAWN>()
-                    + 12 * outflanking
+    int complexity =  12 * (pe->pawn_asymmetry() + pos.count<PAWN>() + outflanking)
                     + 16 * pawnsOnBothFlanks
                     + 48 * !pos.non_pawn_material()
                     -118 ;
