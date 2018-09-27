e671ca3c29c6545ea95578e73b33dfa9554a5f4e
protonspring <mike@whiteley.org>
2018-09-27 12:27:09 -0600

    mg -4

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -887,7 +887,7 @@ void init() {
 
   for (int m = 0; m < 32; ++m)
   {
-    MobilityBonus[QUEEN-2][m] = make_score(47-120*(1-log10(m+2)),47-270*(1-log10(m+5)));
+    MobilityBonus[QUEEN-2][m] = make_score(39-120*(1-log10(m+2)),47-270*(1-log10(m+5)));
   //std::cout << "<" << mg_value(MobilityBonus[QUEEN-2][m]) << ","
                    //<< eg_value(MobilityBonus[QUEEN-2][m]) << ">" << std::endl;
   }
