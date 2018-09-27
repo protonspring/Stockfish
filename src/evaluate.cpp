450fc9e17155dc819337a827b5a93b42ed69f003
protonspring <mike@whiteley.org>
2018-09-27 08:03:26 -0600

    2,2

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -642,7 +642,7 @@ namespace {
             score -= HinderPassedPawn;
 
         if (pe->pawn_attacks(Us) & s)
-            score += make_score(0,5);
+            score += make_score(2,2);
 
         int r = relative_rank(Us, s);
         int w = PassedDanger[r];
