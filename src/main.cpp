e1c2a2f519f408d81b44845d674044bbb39454d2
protonspring <mike@whiteley.org>
2018-09-28 21:37:26 -0600

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,7 @@
 #include "position.h"
 #include "search.h"
 #include "thread.h"
+#include "evaluate.h"
 #include "tt.h"
 #include "uci.h"
 #include "syzygy/tbprobe.h"
@@ -43,6 +44,7 @@ int main(int argc, char* argv[]) {
   Bitbases::init();
   Search::init();
   Pawns::init();
+  Eval::init();
   Tablebases::init(Options["SyzygyPath"]); // After Bitboards are set
   Threads.set(Options["Threads"]);
   Search::clear(); // After threads are up
