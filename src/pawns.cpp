b0b748ff0cd5fa2d51895b6c0e0801fb96278f49
protonspring <mike@whiteley.org>
2018-10-08 08:42:51 -0600

    fully passed.

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -35,6 +35,7 @@ namespace {
   constexpr Score Backward = S( 9, 24);
   constexpr Score Doubled  = S(11, 56);
   constexpr Score Isolated = S( 5, 15);
+  constexpr Score FullyPassed = S(10, 10);
 
   // Connected pawn bonus by opposed, phalanx, #support and rank
   Score Connected[2][2][3][RANK_NB];
@@ -143,6 +144,10 @@ namespace {
 
         if (doubled && !supported)
             score -= Doubled;
+
+        //extra bonus if fully passed
+        if (!stoppers)
+            score += FullyPassed;
     }
 
     return score;
