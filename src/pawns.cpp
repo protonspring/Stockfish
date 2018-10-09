2d480ecea0a112c4365da45f29dba3708910a3ca
protonspring <mike@whiteley.org>
2018-10-09 08:11:42 -0600

    1,4

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -37,7 +37,7 @@ namespace {
   constexpr Score Isolated = S( 5, 15);
 
   // Pseudo passed, or Fully passed
-  constexpr Score Passed[2] = { S( 1, 2), S( 1, 6)};
+  constexpr Score FullyPassed = S( 1, 4);
 
   // Connected pawn bonus by opposed, phalanx, #support and rank
   Score Connected[2][2][3][RANK_NB];
@@ -123,10 +123,7 @@ namespace {
         if (   !(stoppers ^ lever ^ leverPush)
             && popcount(supported) >= popcount(lever) - 1
             && popcount(phalanx)   >= popcount(leverPush))
-        {
             e->passedPawns[Us] |= s;
-            score += Passed[!stoppers];
-        }
 
         else if (   stoppers == SquareBB[s + Up]
                  && relative_rank(Us, s) >= RANK_5)
@@ -134,10 +131,7 @@ namespace {
             b = shift<Up>(supported) & ~theirPawns;
             while (b)
                 if (!more_than_one(theirPawns & PawnAttacks[Us][pop_lsb(&b)]))
-                {
                     e->passedPawns[Us] |= s;
-                    score += Passed[!stoppers];
-                }
         }
 
         // Score this pawn
@@ -152,6 +146,9 @@ namespace {
 
         if (doubled && !supported)
             score -= Doubled;
+
+        if (!stoppers)
+            score += FullyPassed;
     }
 
     return score;
