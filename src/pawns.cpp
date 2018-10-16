a0994e5e7308a89f3923a54a5f63e7dfb088b60d
protonspring <mike@whiteley.org>
2018-10-16 10:42:22 -0600

    combine passed rules.

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -118,8 +118,7 @@ namespace {
         // which could become passed after one or two pawn pushes when are
         // not attacked more times than defended.
         if (   !(stoppers ^ lever ^ leverPush)
-            && popcount(supported) >= popcount(lever) - 1
-            && popcount(phalanx)   >= popcount(leverPush))
+              && popcount(supported | phalanx) >= popcount(lever | leverPush))
             e->passedPawns[Us] |= s;
 
         else if (   stoppers == SquareBB[s + Up]
