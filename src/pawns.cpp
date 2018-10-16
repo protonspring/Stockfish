ef5983708961ce9beb9a635a872f20583a59d2b9
protonspring <mike@whiteley.org>
2018-10-16 17:29:35 -0600

    v3

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -117,11 +117,9 @@ namespace {
         // full attack info to evaluate them. Include also not passed pawns
         // which could become passed after one or two pawn pushes when are
         // not attacked more times than defended.
-        if (   !(stoppers ^ lever ^ leverPush)
-            && popcount(supported) >= popcount(lever) - 1
-            && popcount(phalanx)   >= popcount(leverPush))
+        if ( (!(stoppers ^ lever)     && (popcount(supported) >= popcount(lever))) ||
+             (!(stoppers ^ leverPush) && (popcount(phalanx) >= popcount(leverPush))))
             e->passedPawns[Us] |= s;
-
         else if (   stoppers == SquareBB[s + Up]
                  && relative_rank(Us, s) >= RANK_5)
         {
