8e25da53a263d706cc2410d52f3dfca52cd465ce
protonspring <mike@whiteley.org>
2018-10-16 14:53:21 -0600

    fix

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -118,7 +118,7 @@ namespace {
         // which could become passed after one or two pawn pushes when are
         // not attacked more times than defended.
         if (  !(stoppers ^ lever) ||
-             (!(stoppers ^ leverPush) && (popcount(phalanx) >= popcount(leverPush)))
+             (!(stoppers ^ leverPush) && (popcount(phalanx) >= popcount(leverPush))))
             e->passedPawns[Us] |= s;
         else if (   stoppers == SquareBB[s + Up]
                  && relative_rank(Us, s) >= RANK_5)
