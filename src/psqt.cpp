0d2049580e150891f5205a49fc3bd4dcb7129f2a
protonspring <mike@whiteley.org>
2018-10-25 16:02:39 -0600

    kings, v1.

--- src/psqt.cpp
+++ src/psqt.cpp
@@ -108,6 +108,13 @@ Score psq[PIECE_NB][SQUARE_NB];
 // tables are initialized by flipping and changing the sign of the white scores.
 void init() {
 
+  for (int r = RANK_1; r <= RANK_8; r++)
+     for (int f = FILE_A; f < FILE_E; f++)
+     {
+        Bonus[KING][r][f] = make_score (100+7*pow(f-3.5,2)+7*pow(r-3.5,2),
+                                        190-8*pow(f-3.5,2)-8*pow(r-3.5,2));
+     }
+
   for (Piece pc = W_PAWN; pc <= W_KING; ++pc)
   {
       PieceValue[MG][~pc] = PieceValue[MG][pc];
@@ -122,12 +129,6 @@ void init() {
           psq[~pc][~s] = -psq[pc][s];
       }
   }
-
-  for (int f = FILE_A; f < FILE_E; f++)
-     for (int r = RANK_1; r <= RANK_8; r++)
-     {
-        Bonus[PAWN][r][f] = make_score (-10 + 3*f + 4*r, -10 + 3*f + 4*r);
-     }
 }
 
 } // namespace PSQT
