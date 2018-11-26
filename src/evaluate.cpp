1b76acde92e7f0c8f75e3139e9865deaececfc1f
protonspring <mike@whiteley.org>
2018-11-26 11:09:21 -0700

    offset file.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -147,8 +147,8 @@ namespace {
 
   // PassedFile[File] contains a bonus according to the file of a passed pawn
   constexpr Score PassedFile[FILE_NB] = {
-    S( -1,  7), S( 0,  9), S(-9, -8), S(-30,-14),
-    S(-30,-14), S(-9, -8), S( 0,  9), S( -1,  7)
+    S( -1-5,  7-5), S( 0-5,  9-5), S(-9-5, -8-5), S(-30-5,-14-5),
+    S(-30-5,-14-5), S(-9-5, -8-5), S( 0-5,  9-5), S( -1-5,  7-5)
   };
 
   // Assorted bonuses and penalties
