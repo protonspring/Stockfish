f4fff8f7bd64d9dea1584cccfebd4d0d3f12a144
protonspring <mike@whiteley.org>
2018-11-28 07:56:39 -0700

    replace thread arrays.

--- src/search.cpp
+++ src/search.cpp
@@ -61,10 +61,6 @@ namespace {
   // Different node types, used as a template parameter
   enum NodeType { NonPV, PV };
 
-  // Sizes and phases of the skip-blocks, used for distributing search depths across the threads
-  constexpr int SkipSize[]  = { 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4 };
-  constexpr int SkipPhase[] = { 0, 1, 0, 1, 2, 3, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 6, 7 };
-
   // Razor and futility margins
   constexpr int RazorMargin = 600;
   Value futility_margin(Depth d, bool improving) {
@@ -353,8 +349,8 @@ void Thread::search() {
       // Distribute search depths across the helper threads
       if (idx > 0)
       {
-          int i = (idx - 1) % 20;
-          if (((rootDepth / ONE_PLY + SkipPhase[i]) / SkipSize[i]) % 2)
+          int skipSize = sqrt(idx + 1);  //skip block sizes by thread number
+          if (((rootDepth / ONE_PLY + idx) / skipSize) % 2)
               continue;  // Retry with an incremented rootDepth
       }
 
