1192fd780fc2b0f24decfc08d9fcf5840a168538
protonspring <mike@whiteley.org>
2018-12-01 09:01:43 -0700

    bonus for advancing in enemy king flank.

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -35,6 +35,7 @@ namespace {
   constexpr Score Backward = S( 9, 24);
   constexpr Score Doubled  = S(11, 56);
   constexpr Score Isolated = S( 5, 15);
+  constexpr Score KingFlankBonus = S( 1,  1);
 
   // Connected pawn bonus by opposed, phalanx, #support and rank
   Score Connected[2][2][3][RANK_NB];
@@ -139,6 +140,10 @@ namespace {
 
         if (doubled && !supported)
             score -= Doubled;
+
+	//bonus for advancing in the enemy's king flank
+	if (KingFlank[file_of(pos.square<KING>(Them))] & s)
+            score += KingFlankBonus * relative_rank(Us, s);
     }
 
     return score;
