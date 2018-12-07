53b6036f37a9d194dd9af16101051ef66813c23d
protonspring <mike@whiteley.org>
2018-12-06 23:43:31 -0700

    v2

--- src/position.cpp
+++ src/position.cpp
@@ -591,17 +591,13 @@ bool Position::pseudo_legal(const Move m) const {
   if (type_of(m) != NORMAL)
       return MoveList<LEGAL>(*this).contains(m);
 
-  // Is not a promotion, so promotion piece must be empty
-  if (promotion_type(m) - KNIGHT != NO_PIECE_TYPE)
-      return false;
-
+  // The destination square cannot be occupied by a friendly piece
   // If the 'from' square is not occupied by a piece belonging to the side to
   // move, the move is obviously not legal.
-  if (pc == NO_PIECE || color_of(pc) != us)
-      return false;
-
-  // The destination square cannot be occupied by a friendly piece
-  if (pieces(us) & to)
+  // Is not a promotion, so promotion piece must be empty
+  if ((pieces(us) & to) ||
+      (pc == NO_PIECE || color_of(pc) != us) ||
+      (promotion_type(m) - KNIGHT != NO_PIECE_TYPE))
       return false;
 
   // Handle the special case of a pawn move
