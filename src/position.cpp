10813bc4088c4373cf712bd26de2ae9e45befa4d
protonspring <mike@whiteley.org>
2018-12-10 19:30:32 -0700

    fixed.

--- src/position.cpp
+++ src/position.cpp
@@ -503,8 +503,11 @@ Bitboard Position::slider_blockers(Square s, Bitboard& pinners) const {
   // Snipers are enemy pieces that attack 's' when a piece is removed
   // We don't consider pieces of the same values as the piece on s.
   Bitboard snipers = ((PseudoAttacks[  ROOK][s] & pieces(them, QUEEN, ROOK))
-                    | (PseudoAttacks[BISHOP][s] & pieces(them, QUEEN, BISHOP)))
-                    ^ type_of(piece_on(s));
+                    | (PseudoAttacks[BISHOP][s] & pieces(them, QUEEN, BISHOP)));
+
+  if (type_of(piece_on(s)) == QUEEN)
+      snipers ^= pieces(QUEEN);
+
   while (snipers)
   {
     Square sniperSq = pop_lsb(&snipers);
