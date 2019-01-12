f53e274793e5ebfbaacddbbc43d12818282ea5fc
protonspring <mike@whiteley.org>
2019-01-11 22:12:55 -0700

    knight moves first.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -828,8 +828,8 @@ namespace {
     initialize<BLACK>();
 
     // Pieces should be evaluated first (populate attack tables)
-    score +=  pieces<WHITE, KNIGHT>() - pieces<BLACK, KNIGHT>()
-            + pieces<WHITE, BISHOP>() - pieces<BLACK, BISHOP>()
+    score +=  pieces<WHITE, KNIGHT>() - pieces<BLACK, KNIGHT>();
+    score +=  pieces<WHITE, BISHOP>() - pieces<BLACK, BISHOP>()
             + pieces<WHITE, ROOK  >() - pieces<BLACK, ROOK  >()
             + pieces<WHITE, QUEEN >() - pieces<BLACK, QUEEN >();
 
