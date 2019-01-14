a1eccb915ea32f1630d10ff9c1ea37d339c61ca9
protonspring <mike@whiteley.org>
2019-01-14 09:01:34 -0700

    try to remove capture histories.

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -107,8 +107,8 @@ void MovePicker::score() {
 
   for (auto& m : *this)
       if (Type == CAPTURES)
-          m.value =  PieceValue[MG][pos.piece_on(to_sq(m))]
-                       - Value(type_of(pos.moved_piece(m))) / 32;
+          m.value =  PieceValue[MG][pos.piece_on(to_sq(m))] / 2
+                   - PieceValue[MG][pos.moved_piece(m)];
 
       else if (Type == QUIETS)
           m.value =  (*mainHistory)[pos.side_to_move()][from_to(m)]
