26ce8fe90cfc2fc9f779ebd55cb65c1df1330995
protonspring <mike@whiteley.org>
2019-01-28 15:51:13 -0700

    remove term from stat bonus.

--- src/search.cpp
+++ src/search.cpp
@@ -81,8 +81,8 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth depth) {
-    int d = depth / ONE_PLY;
-    return d > 17 ? 0 : 29 * d * d + 138 * d - 134;
+    int d = depth / ONE_PLY + 3;
+    return d > 20 ? 0 : 27 * d * d - 440;
   }
 
   // Add a small random component to draw evaluations to keep search dynamic
