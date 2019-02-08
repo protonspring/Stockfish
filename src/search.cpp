2498a9cc33e1ec7c8acde333fbb6f6d506e101d7
protonspring <mike@whiteley.org>
2019-02-08 13:20:06 -0700

    simple sigmoid.

--- src/search.cpp
+++ src/search.cpp
@@ -496,8 +496,6 @@ void Thread::search() {
           && !Threads.stop
           && !mainThread->stopOnPonderhit)
       {
-          //int diff = (mainThread->previousScore - bestValue);
-          //double fallingEval = diff < -1 ? 0.5 : diff > 62 ? 1.5 : .527 + 0.0155 * diff;
           double mid = 0.125 * double(mainThread->previousScore - bestValue - 30);
           double fallingEval = 1.0 + 0.6 * (mid / (1 + abs(mid)));
 
