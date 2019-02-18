bb77e333d57ecf28fab188e97338826608beb43d
protonspring <mike@whiteley.org>
2019-02-18 09:34:49 -0700

    more bonus if opponent has no minor pieces.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -317,12 +317,24 @@ namespace {
             // Bonus if piece is on an outpost square or can reach one
             bb = OutpostRanks & ~pe->pawn_attacks_span(Them);
             if (bb & s)
+            {
                 score += Outpost * (Pt == KNIGHT ? 4 : 2)
                                  * (1 + bool(attackedBy[Us][PAWN] & s));
 
+                //more bonus if opponent has no minor pieces
+                if (!pos.count<KNIGHT>(Them) && !pos.count<BISHOP>(Them))
+                    score += Outpost;
+            }
+
             else if (bb &= b & ~pos.pieces(Us))
+            {
                 score += Outpost * (Pt == KNIGHT ? 2 : 1)
                                  * (1 + bool(attackedBy[Us][PAWN] & bb));
+ 
+                //more bonus if opponent has no minor pieces
+                if (!pos.count<KNIGHT>(Them) && !pos.count<BISHOP>(Them))
+                    score += Outpost;
+            }
 
             // Knight and Bishop bonus for being right behind a pawn
             if (shift<Down>(pos.pieces(PAWN)) & s)
