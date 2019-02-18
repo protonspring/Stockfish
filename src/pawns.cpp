ef524205364474264bb22d5d909ec41b57c1c296
protonspring <mike@whiteley.org>
2019-02-18 09:25:08 -0700

    5,0

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -35,7 +35,7 @@ namespace {
   constexpr Score Backward = S( 9, 24);
   constexpr Score Doubled  = S(11, 56);
   constexpr Score Isolated = S( 5, 15);
-  constexpr Score DoubleLever = S(5, 5);
+  constexpr Score DoubleLever = S(5, 0);
 
   // Connected pawn bonus by opposed, phalanx, #support and rank
   Score Connected[2][2][3][RANK_NB];
