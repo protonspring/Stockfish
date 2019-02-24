cb67289396c9787cb47d201005b313daa4f7fd29
protonspring <mike@whiteley.org>
2019-02-24 14:01:21 -0700

    simplify seed.

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -161,9 +161,7 @@ void init() {
           for (int support = 0; support <= 2; ++support)
               for (Rank r = RANK_2; r < RANK_8; ++r)
   {
-      int v = 17 * support;
-      v += (Seed[r] + (phalanx ? (Seed[r + 1] - Seed[r]) / 2 : 0)) >> opposed;
-
+      int v = 17 * support + int(Seed[r] * (1.0 + 0.75 * phalanx)) >> opposed;
       Connected[opposed][phalanx][support][r] = make_score(v, v * (r - 2) / 4);
   }
 }
