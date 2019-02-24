1c2236c2d8c3db640173dd3dec99dbb55079c0c3
protonspring <mike@whiteley.org>
2019-02-24 16:48:12 -0700

    remove seed array.

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -155,16 +155,13 @@ namespace Pawns {
 
 void init() {
 
-  auto seed = [&](int r) { return int(6 * std::pow(1.77,r)); };
-
   for (int opposed = 0; opposed <= 1; ++opposed)
       for (int phalanx = 0; phalanx <= 1; ++phalanx)
           for (int support = 0; support <= 2; ++support)
               for (Rank r = RANK_2; r < RANK_8; ++r)
   {
       int v = 17 * support;
-      v += (seed(r) + (phalanx ? (seed(r + 1) - seed(r)) / 2 : 0)) >> opposed;
-
+      v += int(6 * std::pow(1.77,r) * (1.0 + 0.385 * phalanx)) >> opposed;
       Connected[opposed][phalanx][support][r] = make_score(v, v * (r - 2) / 4);
   }
 }
