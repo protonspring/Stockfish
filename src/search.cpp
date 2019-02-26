b5563f86c57cb4e2dce5d894c57967a61615691e
protonspring <mike@whiteley.org>
2019-02-25 22:07:17 -0700

    remove fmc array.

--- src/search.cpp
+++ src/search.cpp
@@ -72,11 +72,14 @@ namespace {
   }
 
   // Futility and reductions lookup tables, initialized at startup
-  int FutilityMoveCounts[2][16]; // [improving][depth]
   int Reductions[2][64][64];  // [improving][depth][moveNumber]
 
   template <bool PvNode> Depth reduction(bool i, Depth d, int mn) {
     return (Reductions[i][std::min(d / ONE_PLY, 63)][std::min(mn, 63)] - PvNode) * ONE_PLY;
+  }
+
+  constexpr int futility_move_count(bool improving, int depth) {
+    return (5 + depth * depth) * (1 + improving) / 2;
   }
 
   // History and stats update bonus, based on depth
@@ -168,12 +171,6 @@ void Search::init() {
               if (!imp && r > 1.0)
                 Reductions[imp][d][mc]++;
           }
-
-  for (int d = 0; d < 16; ++d)
-  {
-      FutilityMoveCounts[0][d] = int(2.4 + 0.74 * pow(d, 1.78));
-      FutilityMoveCounts[1][d] = int(5.0 + 1.00 * pow(d, 2.00));
-  }
 }
 
 
@@ -915,7 +912,7 @@ moves_loop: // When in check, search starts from here
 
       // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold
       moveCountPruning = depth < 16 * ONE_PLY
-                      && moveCount >= FutilityMoveCounts[improving][depth / ONE_PLY];
+                      && moveCount >= futility_move_count(improving, depth / ONE_PLY);
 
       // Step 13. Extensions (~70 Elo)
 
