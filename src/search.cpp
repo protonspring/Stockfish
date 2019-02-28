6e4602402dd057bf4108bd9e6b1a41e4a3569d95
protonspring <mike@whiteley.org>
2019-02-28 14:13:47 -0700

    remove reductions array.

--- src/search.cpp
+++ src/search.cpp
@@ -73,10 +73,11 @@ namespace {
 
   // Futility and reductions lookup tables, initialized at startup
   int FutilityMoveCounts[2][16]; // [improving][depth]
-  int Reductions[2][64][64];  // [improving][depth][moveNumber]
+  int log_value[256];
 
   template <bool PvNode> Depth reduction(bool i, Depth d, int mn) {
-    return (Reductions[i][std::min(d / ONE_PLY, 63)][std::min(mn, 63)] - PvNode) * ONE_PLY;
+    int r = int(log_value[d] + log_value[mn]) / 2;
+    return Depth(std::round(r) + (r > 0 && !i));
   }
 
   // History and stats update bonus, based on depth
@@ -156,18 +157,8 @@ namespace {
 
 void Search::init() {
 
-  for (int imp = 0; imp <= 1; ++imp)
-      for (int d = 1; d < 64; ++d)
-          for (int mc = 1; mc < 64; ++mc)
-          {
-              double r = log(d) * log(mc) / 1.95;
-
-              Reductions[imp][d][mc] = std::round(r);
-
-              // Increase reduction for non-PV nodes when eval is not improving
-              if (!imp && r > 1.0)
-                Reductions[imp][d][mc]++;
-          }
+  for (int d = 1; d < 256; d++)
+      log_value[d] = std::log(d);
 
   for (int d = 0; d < 16; ++d)
   {
