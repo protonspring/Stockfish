e30491c6a0a81a11b58e05e26e65dac7e4769ba6
protonspring <mike@whiteley.org>
2019-03-17 16:47:27 -0600

    hybrid version

--- src/search.cpp
+++ src/search.cpp
@@ -295,7 +295,7 @@ void Thread::search() {
   Move  lastBestMove = MOVE_NONE;
   Depth lastBestMoveDepth = DEPTH_ZERO;
   MainThread* mainThread = (this == Threads.main() ? Threads.main() : nullptr);
-  bool timeReduction = false;
+  double timeReduction = 1.0;
   Color us = rootPos.side_to_move();
 
   std::memset(ss-7, 0, 10 * sizeof(Stack));
@@ -483,8 +483,9 @@ void Thread::search() {
           fallingEval        = std::max(0.5, std::min(1.5, fallingEval));
 
           // If the bestMove is stable over several iterations, reduce time accordingly
-          timeReduction = lastBestMoveDepth + 10 * ONE_PLY < completedDepth;
-          double reduction = (1 + 0.422782*mainThread->previousTimeReduction) * (1 - 0.487179*timeReduction);
+          timeReduction =  0.03 * int(completedDepth - lastBestMoveDepth);
+
+          double reduction = (1 + 0.423*mainThread->previousTimeReduction) * (1 - timeReduction);
 
           // Use part of the gained time from a previous stable move for the current move
           double bestMoveInstability = 1.0 + mainThread->bestMoveChanges;
@@ -506,7 +507,7 @@ void Thread::search() {
   if (!mainThread)
       return;
 
-  mainThread->previousTimeReduction = timeReduction;
+  mainThread->previousTimeReduction = (timeReduction > 0.3);
 
   // If skill level is enabled, swap best PV line with the sub-optimal one
   if (skill.enabled())
