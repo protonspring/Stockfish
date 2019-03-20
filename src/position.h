8b9c3bed75841d8a5c955cf34a8d2bc06d6d0ecb
protonspring <mike@whiteley.org>
2019-03-20 11:34:05 -0600

    simplify pawns_on_same_color

--- src/position.h
+++ src/position.h
@@ -128,6 +128,7 @@ public:
   // Piece specific
   bool pawn_passed(Color c, Square s) const;
   bool opposite_bishops() const;
+  Bitboard pawns_on_same_color(Color us, Square s) const;
 
   // Doing and undoing moves
   void do_move(Move m, StateInfo& newSt);
@@ -354,6 +355,10 @@ inline bool Position::opposite_bishops() const {
   return   pieceCount[W_BISHOP] == 1
         && pieceCount[B_BISHOP] == 1
         && opposite_colors(square<BISHOP>(WHITE), square<BISHOP>(BLACK));
+}
+
+inline Bitboard Position::pawns_on_same_color(Color us, Square s) const {
+  return pieces(us, PAWN) & (DarkSquares & s ? DarkSquares : ~DarkSquares);
 }
 
 inline bool Position::is_chess960() const {
