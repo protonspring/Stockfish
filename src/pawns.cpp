97369085ed7b26636c58455816c5aa8da2c9c34f
protonspring <mike@whiteley.org>
2019-03-23 10:30:19 -0600

    just ksq +/- 1

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -245,12 +245,10 @@ Score Entry::do_king_safety(const Position& pos) {
 
   Value bonus = evaluate_shelter<Us>(pos, ksq);
 
-  // If we can castle use the bonus after the castling if it is bigger
-  if (pos.can_castle(Us | KING_SIDE))
-      bonus = std::max(bonus, evaluate_shelter<Us>(pos, relative_square(Us, SQ_G1)));
-
-  if (pos.can_castle(Us | QUEEN_SIDE))
-      bonus = std::max(bonus, evaluate_shelter<Us>(pos, relative_square(Us, SQ_C1)));
+  if (~FileABB & ksq)
+      bonus = std::max(bonus, evaluate_shelter<Us>(pos, ksq-1);
+  if (~FileHBB & ksq)
+      bonus = std::max(bonus, evaluate_shelter<Us>(pos, ksq+1);
 
   return make_score(bonus, -16 * minKingPawnDistance);
 }
