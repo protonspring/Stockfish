5d2fc5407565cb8fcbd37502d2aee80990e14ff2
protonspring <mike@whiteley.org>
2019-03-27 10:01:07 -0600

    fix.

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -83,7 +83,7 @@ void Bitboards::init() {
       for (Square s2 = SQ_A1; s2 <= SQ_H8; ++s2)
           {
               SquareDistance[s1][s2] = std::max(distance<File>(s1, s2), distance<Rank>(s1, s2));
-              DistanceRingBB[s1][SquareDistance[s1][s2]] |= s2;
+              DistanceRingBB[s1][SquareDistance[s1][s2]] |= (1ULL << s2);
           }
 
   int steps[][5] = { {}, { 7, 9 }, { 6, 10, 15, 17 }, {}, {}, {}, { 1, 7, 8, 9 } };
