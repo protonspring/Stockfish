bb5ec1bac962fc0d25bc9ce0aa35bba307216318
protonspring <mike@whiteley.org>
2019-04-09 10:24:16 -0600

    use between_bb instead of BetweenBB

--- src/position.cpp
+++ src/position.cpp
@@ -340,8 +340,8 @@ void Position::set_castling_right(Color c, Square rfrom) {
   Square kto = relative_square(c, cs == KING_SIDE ? SQ_G1 : SQ_C1);
   Square rto = relative_square(c, cs == KING_SIDE ? SQ_F1 : SQ_D1);
 
-  castlingPath[cr] = (BetweenBB[rfrom][rto] | BetweenBB[kfrom][kto] | rto | kto)
-                   & ~(SquareBB[kfrom] | rfrom);
+  castlingPath[cr] = (between_bb(rfrom, rto) | between_bb(kfrom, kto) | rto | kto)
+                   & ~(square_bb(kfrom) | rfrom);
 }
 
 
