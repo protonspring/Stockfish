1ff4a6f7e55613929b0458f41d4d2f8d36b7c038
protonspring <mike@whiteley.org>
2019-04-09 14:09:14 -0600

    more removes.

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -77,12 +77,10 @@ void Bitboards::init() {
   for (unsigned i = 0; i < (1 << 16); ++i)
       PopCnt16[i] = std::bitset<16>(i).count();
 
-  for (Square s = SQ_A1; s <= SQ_H8; ++s)
-      DistanceRingBB[0][s] = (1ULL << s);
-
   for (Square s1 = SQ_A1; s1 <= SQ_H8; ++s1)
       for (Square s2 = SQ_A1; s2 <= SQ_H8; ++s2)
           {
+              DistanceRingBB[0][s2] = (1ULL << s2);
               SquareDistance[s1][s2] = std::max(distance<File>(s1, s2), distance<Rank>(s1, s2));
               DistanceRingBB[SquareDistance[s1][s2]][s1] |= s2;
           }
