b03f6e018590c4c0bfe9df341cf7956b3417eb7f
protonspring <mike@whiteley.org>
2019-04-26 10:36:27 -0600

    -40

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -471,7 +471,7 @@ namespace {
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  +   5 * kingFlankAttacks * kingFlankAttacks / 16
-                 -   35;
+                 -   55;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
