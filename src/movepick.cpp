9c338c4464c67dcfe53be6f7ac74b1af5fc5c760
protonspring <mike@whiteley.org>
2019-05-02 13:01:32 -0600

    simplify move scoring.

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -124,7 +124,6 @@ void MovePicker::score() {
                        - Value(type_of(pos.moved_piece(m)));
           else
               m.value =  (*mainHistory)[pos.side_to_move()][from_to(m)]
-                       + (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
                        - (1 << 28);
       }
 }
