0e89e2c459b652b2ee4549d8479eb216c6834d26
protonspring <mike@whiteley.org>
2019-05-13 13:38:16 -0600

    with lower base outpost value.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -141,7 +141,7 @@ namespace {
   constexpr Score KnightOnQueen      = S( 16, 12);
   constexpr Score LongDiagonalBishop = S( 45,  0);
   constexpr Score MinorBehindPawn    = S( 18,  3);
-  constexpr Score Outpost            = S(  9,  3);
+  constexpr Score Outpost            = S(  7,  2);
   constexpr Score PawnlessFlank      = S( 17, 95);
   constexpr Score RestrictedPiece    = S(  7,  7);
   constexpr Score RookOnPawn         = S( 10, 32);
