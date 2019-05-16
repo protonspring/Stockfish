542c5a25de6ab56d7030f41a2612a615e08fd1b8
protonspring <mike@whiteley.org>
2019-05-16 12:11:38 -0600

    +5

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -744,7 +744,7 @@ namespace {
                     + 11 * pos.count<PAWN>()
                     +  9 * outflanking
                     + 49 * !pos.non_pawn_material()
-                     -94;
+                     -98;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
