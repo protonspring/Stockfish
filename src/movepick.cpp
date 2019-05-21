ba3e4e2d35ee1f663d8f669a767a037347634d76
protonspring <mike@whiteley.org>
2019-05-21 12:20:55 -0600

    no threshold for probcut.

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -237,7 +237,7 @@ top:
       return select<Best>([](){ return true; });
 
   case PROBCUT:
-      return select<Best>([&](){ return pos.see_ge(*cur, threshold); });
+      return select<Best>([&](){ return pos.see_ge(*cur); });
 
   case QCAPTURE:
       if (select<Best>([&](){ return   depth > DEPTH_QS_RECAPTURES
