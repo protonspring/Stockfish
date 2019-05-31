af27288ebad69303781ad3b66eeffd715f3a580b
protonspring <mike@whiteley.org>
2019-05-31 09:09:18 -0600

    not backward if advanced.

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -104,7 +104,8 @@ namespace {
         // A pawn is backward when it is behind all pawns of the same color
         // on the adjacent files and cannot be safely advanced.
         backward =  !(ourPawns & pawn_attack_span(Them, s + Up))
-                  && (stoppers & (leverPush | (s + Up)));
+                  && (stoppers & (leverPush | (s + Up)))
+                  && (r < relative_rank(Us, RANK_4));
 
         // Passed pawns will be properly scored in evaluation because we need
         // full attack info to evaluate them. Include also not passed pawns
