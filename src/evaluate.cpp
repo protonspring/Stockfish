44039d6edff302c0fd9329c16db26f663a7f22fc
protonspring <mike@whiteley.org>
2019-06-01 23:09:50 -0600

    simplify WeakUnopposedPawn.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -558,8 +558,7 @@ namespace {
     score += RestrictedPiece * popcount(b);
 
     // Bonus for enemy unopposed weak pawns
-    if (pos.pieces(Us, ROOK, QUEEN))
-        score += WeakUnopposedPawn * pe->weak_unopposed(Them);
+    score += WeakUnopposedPawn * pe->weak_unopposed(Them);
 
     // Find squares where our pawns can push on the next move
     b  = shift<Up>(pos.pieces(Us, PAWN)) & ~pos.pieces();
