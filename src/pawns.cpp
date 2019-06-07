d379820a996b0b1a39a63540df887b2adc33d032
U-USURF\mtwhiteley <mtwhiteley@C01863.usurf.usu.edu>
2019-06-06 23:02:27 -0700

    more passed.

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -117,7 +117,7 @@ namespace {
 
         else if (stoppers == square_bb(s + Up) && r >= RANK_5)
         {
-            b = shift<Up>(support) & ~theirPawns;
+            b = phalanx | (shift<Up>(support) & ~theirPawns);
             while (b)
                 if (!more_than_one(theirPawns & PawnAttacks[Us][pop_lsb(&b)]))
                     e->passedPawns[Us] |= s;
