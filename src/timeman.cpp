10447c185a1b135694daa1d6f6c7aae256be7ee0
protonspring <mike@whiteley.org>
2019-06-11 09:23:35 -0600

    15/16

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -63,6 +63,8 @@ namespace {
     for (int i = 1; i < movesToGo; ++i)
         otherMovesImportance += move_importance(ply + 2 * i);
 
+    otherMovesImportance = 15 * otherMovesImportance / 16;
+
     double ratio1 = (TMaxRatio * moveImportance) / (TMaxRatio * moveImportance + otherMovesImportance);
     double ratio2 = (moveImportance + TStealRatio * otherMovesImportance) / (moveImportance + otherMovesImportance);
 
