80ba307e8166109d66474c1e420ee3b042300d55
protonspring <mike@whiteley.org>
2019-06-12 14:37:10 -0600

    only advanced ranks for minorBehindPawn.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -310,7 +310,7 @@ namespace {
                 score += Outpost / (Pt == KNIGHT ? 1 : 2);
 
             // Knight and Bishop bonus for being right behind a pawn
-            if (shift<Down>(pos.pieces(PAWN)) & s)
+            if ((shift<Down>(pos.pieces(PAWN)) & s) && (relative_rank(Us, s) > RANK_3))
                 score += MinorBehindPawn;
 
             // Penalty if the piece is far from the king
