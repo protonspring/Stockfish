9eb43e055b04c6e8f31a9819436dafeee6859091
protonspring <mike@whiteley.org>
2019-06-24 22:24:06 -0600

    3,8

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -35,7 +35,7 @@ namespace {
   constexpr Score Backward = S( 9, 24);
   constexpr Score Doubled  = S(11, 56);
   constexpr Score Isolated = S( 5, 15);
-  constexpr Score DoubleIsolated = S( 5, 10);
+  constexpr Score DoubleIsolated = S( 3,  8);
   constexpr Score WeakUnopposed = S( 13, 27);
 
   // Connected pawn bonus
