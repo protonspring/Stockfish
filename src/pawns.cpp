4daa338b1f0242c1042bd255e0da47bee4f4a7cc
protonspring <mike@whiteley.org>
2019-07-01 16:45:49 -0600

    v-connected

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -135,7 +135,7 @@ namespace {
             int v =  Connected[r] * (phalanx ? 3 : 2) / (opposed ? 2 : 1)
                    + 17 * popcount(support);
 
-            score += make_score(v, v);
+            score += make_score(v, v - Connected[r]);
         }
         else if (!neighbours)
             score -= Isolated + WeakUnopposed * int(!opposed);
