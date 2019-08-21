ecea14cc3a1e042e9c4d2959fb2d649bd8e31bec
protonspring <mike@whiteley.org>
2019-08-21 08:00:24 -0600

    188

--- src/search.cpp
+++ src/search.cpp
@@ -64,7 +64,7 @@ namespace {
   // Razor and futility margins
   constexpr int RazorMargin = 661;
   Value futility_margin(Depth d, bool improving) {
-    return Value(198 * (d / ONE_PLY - improving));
+    return Value(188 * (d / ONE_PLY - improving));
   }
 
   // Reductions lookup table, initialized at startup
