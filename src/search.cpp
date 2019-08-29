3bcf1a8bc26e88fed313f22c27856b409fc8d572
protonspring <mike@whiteley.org>
2019-08-29 11:21:18 -0600

    clamp values.

--- src/search.cpp
+++ src/search.cpp
@@ -151,8 +151,8 @@ namespace {
   template <NodeType NT>
   Value qsearch(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth = DEPTH_ZERO);
 
-  Value value_to_tt(Value v, int ply);
-  Value value_from_tt(Value v, int ply);
+  inline Value value_to_tt(Value v, int ply);
+  constexpr Value value_from_tt(Value v, int ply);
   void update_pv(Move* pv, Move move, Move* childPv);
   void update_continuation_histories(Stack* ss, Piece pc, Square to, int bonus);
   void update_quiet_stats(const Position& pos, Stack* ss, Move move, Move* quiets, int quietCount, int bonus);
@@ -1523,12 +1523,11 @@ moves_loop: // When in check, search starts from here
   // "plies to mate from the current position". Non-mate scores are unchanged.
   // The function is called before storing a value in the transposition table.
 
-  Value value_to_tt(Value v, int ply) {
+  inline Value value_to_tt(Value v, int ply) {
 
     assert(v != VALUE_NONE);
 
-    return  v >= VALUE_MATE_IN_MAX_PLY  ? v + ply
-          : v <= VALUE_MATED_IN_MAX_PLY ? v - ply : v;
+    return clamp(v, VALUE_MATED_IN_MAX_PLY - ply, VALUE_MATE_IN_MAX_PLY + 	ply);
   }
 
 
@@ -1536,11 +1535,8 @@ moves_loop: // When in check, search starts from here
   // from the transposition table (which refers to the plies to mate/be mated
   // from current position) to "plies to mate/be mated from the root".
 
-  Value value_from_tt(Value v, int ply) {
-
-    return  v == VALUE_NONE             ? VALUE_NONE
-          : v >= VALUE_MATE_IN_MAX_PLY  ? v - ply
-          : v <= VALUE_MATED_IN_MAX_PLY ? v + ply : v;
+  constexpr Value value_from_tt(Value v, int ply) {
+    return clamp(v, VALUE_MATED_IN_MAX_PLY - ply, VALUE_MATE_IN_MAX_PLY + ply);
   }
 
 
