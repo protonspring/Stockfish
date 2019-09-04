e30aebaf9e9e9b14022a4f64e6c39f9d7969ef5c
protonspring <mike@whiteley.org>
2019-09-03 23:25:24 -0600

    82, 41

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -33,7 +33,7 @@ namespace {
 
   // Pawn penalties
   constexpr Score Backward      = S( 9, 24);
-  constexpr Score BlockedStorm  = S(41, 82);
+  constexpr Score BlockedStorm  = S(82, 41);
   constexpr Score Doubled       = S(11, 56);
   constexpr Score Isolated      = S( 5, 15);
   constexpr Score WeakLever     = S( 0, 56);
