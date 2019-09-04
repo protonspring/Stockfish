8c8efc3f5c2b42e935cbaee5541df5e7a853c30e
protonspring <mike@whiteley.org>
2019-09-04 14:03:57 -0600

    remove passed rank array.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -121,11 +121,6 @@ namespace {
     S(0, 0), S(0, 24), S(38, 71), S(38, 61), S(0, 38), S(51, 38)
   };
 
-  // PassedRank[Rank] contains a bonus according to the rank of a passed pawn
-  constexpr Score PassedRank[RANK_NB] = {
-    S(0, 0), S(10, 28), S(17, 33), S(15, 41), S(62, 72), S(168, 177), S(276, 260)
-  };
-
   // Assorted bonuses and penalties
   constexpr Score BishopPawns        = S(  3,  7);
   constexpr Score CorneredBishop     = S( 50, 50);
@@ -609,8 +604,8 @@ namespace {
         int r = relative_rank(Us, s);
         File f = file_of(s);
 
-        Score bonus = PassedRank[r];
-
+        Score bonus = r < RANK_5 ? make_score(  4 * 4 +   7,  5 * r +  15)
+                                 : make_score(107 * r - 366, 97 * r - 315);
         if (r > RANK_3)
         {
             int w = 5 * r - 13;
