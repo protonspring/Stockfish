2583008ee6344f11ab3acf6e99384aa2faa8a11c
protonspring <mike@whiteley.org>
2019-09-08 22:02:27 -0600

    -5

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -714,7 +714,7 @@ namespace {
   template<Tracing T>
   Score Evaluation<T>::initiative(Value eg) const {
 
-    int outflanking =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK)) - 7;
+    int outflanking =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK)) - 5;
 
     bool pawnsOnBothFlanks =   (pos.pieces(PAWN) & QueenSide)
                             && (pos.pieces(PAWN) & KingSide);
