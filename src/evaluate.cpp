a4aec1f2ec5fe9fa2416709abd83b40c511d81fa
protonspring <mike@whiteley.org>
2019-09-09 08:52:14 -0600

    only distance between kings.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -714,8 +714,7 @@ namespace {
   template<Tracing T>
   Score Evaluation<T>::initiative(Value eg) const {
 
-    int outflanking =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
-                     - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
+    int outflanking = distance(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
 
     bool pawnsOnBothFlanks =   (pos.pieces(PAWN) & QueenSide)
                             && (pos.pieces(PAWN) & KingSide);
