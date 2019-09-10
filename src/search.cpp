0fd198b3fb92ad1eb03721a030a91c8b93ed0fc3
protonspring <mike@whiteley.org>
2019-09-10 11:43:53 -0600

    skip preliminary search.

--- src/search.cpp
+++ src/search.cpp
@@ -871,12 +871,7 @@ namespace {
 
                 pos.do_move(move, st);
 
-                // Perform a preliminary qsearch to verify that the move holds
-                value = -qsearch<NonPV>(pos, ss+1, -raisedBeta, -raisedBeta+1);
-
-                // If the qsearch held, perform the regular search
-                if (value >= raisedBeta)
-                    value = -search<NonPV>(pos, ss+1, -raisedBeta, -raisedBeta+1, depth - 4 * ONE_PLY, !cutNode);
+                value = -search<NonPV>(pos, ss+1, -raisedBeta, -raisedBeta+1, depth - 4 * ONE_PLY, !cutNode);
 
                 pos.undo_move(move);
 
