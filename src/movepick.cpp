ed2ada42fa8aad115ab13e6537bbce4f7f557780
protonspring <mike@whiteley.org>
2019-09-09 22:33:38 -0600

    remove 0 history.

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -112,7 +112,6 @@ void MovePicker::score() {
 
       else if (Type == QUIETS)
           m.value =  (*mainHistory)[pos.side_to_move()][from_to(m)]
-                   + (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
                    + (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    + (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
                    + (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)] / 2;
