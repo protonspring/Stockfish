c3170c82e497bec7dae61a4d41351d361bd33c45
protonspring <mike@whiteley.org>
2019-09-22 20:05:40 -0600

    simplify long diagonal.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -321,8 +321,8 @@ namespace {
                 score -= BishopPawns * pos.pawns_on_same_color_squares(Us, s)
                                      * (1 + popcount(blocked & CenterFiles));
 
-                // Bonus for bishop on a long diagonal which can "see" both center squares
-                if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))
+                // Bonus for bishop which can "see" center squares
+                if (attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center)
                     score += LongDiagonalBishop;
             }
 
