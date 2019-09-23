49fc39cc37a53f8ce54cc8b00b45e8b6758619b0
protonspring <mike@whiteley.org>
2019-09-23 00:52:14 -0600

    simplify long diagonal.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -322,7 +322,7 @@ namespace {
                                      * (1 + popcount(blocked & CenterFiles));
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
-                if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))
+                if (more_than_one(b & Center))
                     score += LongDiagonalBishop;
             }
 
