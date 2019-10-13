d08f217d4cf5c430c81972a22632f92ca5943d40
protonspring <mike@whiteley.org>
2019-10-12 23:37:04 -0600

    simplify complexity.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -712,17 +712,15 @@ namespace {
     bool pawnsOnBothFlanks =   (pos.pieces(PAWN) & QueenSide)
                             && (pos.pieces(PAWN) & KingSide);
 
-    bool almostUnwinnable =   !pe->passed_count()
-                           &&  outflanking < 0
-                           && !pawnsOnBothFlanks;
-
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()
                     + 11 * pos.count<PAWN>()
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    - 36 * almostUnwinnable
+                    - 12 * !pe->passed_count()
+                    - 12 * (outflanking < 0)
+                    - 12 * !pawnsOnBothFlanks
                     -103 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting the
