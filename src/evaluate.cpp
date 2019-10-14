afb1200faadb2d1aee3a70d399ff5888f3583259
protonspring <mike@whiteley.org>
2019-10-13 20:37:53 -0600

    tune simplified outposts.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -135,7 +135,10 @@ namespace {
   constexpr Score KnightOnQueen      = S( 16, 12);
   constexpr Score LongDiagonalBishop = S( 45,  0);
   constexpr Score MinorBehindPawn    = S( 18,  3);
-  constexpr Score Outpost            = S( 32, 10);
+  Score Outpost            = S( 32, 10);
+
+TUNE(Outpost);
+
   constexpr Score PassedFile         = S( 11,  8);
   constexpr Score PawnlessFlank      = S( 17, 95);
   constexpr Score RestrictedPiece    = S(  7,  7);
@@ -295,14 +298,6 @@ namespace {
 
         if (Pt == BISHOP || Pt == KNIGHT)
         {
-            // Bonus if piece is on an outpost square or can reach one
-            bb = OutpostRanks & attackedBy[Us][PAWN] & ~pe->pawn_attacks_span(Them);
-            if (bb & s)
-                score += Outpost * (Pt == KNIGHT ? 2 : 1);
-
-            else if (Pt == KNIGHT && bb & b & ~pos.pieces(Us))
-                score += Outpost;
-
             // Knight and Bishop bonus for being right behind a pawn
             if (shift<Down>(pos.pieces(PAWN)) & s)
                 score += MinorBehindPawn;
@@ -322,6 +317,16 @@ namespace {
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))
                     score += LongDiagonalBishop;
+            }
+            else //KNIGHT
+            {
+                // Bonus if piece is on an outpost square or can reach one
+                bb = OutpostRanks & attackedBy[Us][PAWN] & ~pe->pawn_attacks_span(Them);
+                if (bb & s)
+                    score += Outpost * 2;
+
+                else if (bb & b & ~pos.pieces(Us))
+                    score += Outpost;
             }
 
             // An important Chess960 pattern: A cornered bishop blocked by a friendly
