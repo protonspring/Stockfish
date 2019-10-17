d432bdec8af015a58cd183b156e8d56d2d6076f2
protonspring <mike@whiteley.org>
2019-10-17 09:09:45 -0600

    just s

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -643,7 +643,7 @@ namespace {
         // Scale down bonus for candidate passers which need more than one
         // pawn push to become passed, or have a pawn in front of them.
         if (   !pos.pawn_passed(Us, s + Up)
-            || (pos.pieces(PAWN) & forward_file_bb(Us, (s + Up))))
+            || (pos.pieces(PAWN) & forward_file_bb(Us, s)))
             bonus = bonus / 2;
 
         score += bonus - PassedFile * map_to_queenside(f);
