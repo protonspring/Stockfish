fadcd118a979dfb2e99071f5cd35fc69363bc5cb
protonspring <mike@whiteley.org>
2019-10-27 09:00:34 -0600

    1/2 blocks.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -134,8 +134,8 @@ namespace {
   constexpr Score KingProtector      = S(  7,  8);
   constexpr Score KnightOnQueen      = S( 16, 12);
   constexpr Score LongDiagonalBishop = S( 45,  0);
-  constexpr Score MinorBehindPawn    = S(  9,  2);
-  constexpr Score MinorBlocksPawn    = S( 18,  3);
+  constexpr Score MinorBehindPawn    = S( 18,  3);
+  constexpr Score MinorBlocksPawn    = S(  9,  3);
   constexpr Score Outpost            = S( 32, 10);
   constexpr Score PassedFile         = S( 11,  8);
   constexpr Score PawnlessFlank      = S( 17, 95);
