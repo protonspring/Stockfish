122e1db9e8a9ce667c0252988d8ac164eb2bf2cc
protonspring <mike@whiteley.org>
2019-10-29 17:30:24 -0600

    exclude ksq.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -236,7 +236,7 @@ namespace {
     // Init our king safety tables
     Square s = make_square(clamp(file_of(ksq), FILE_B, FILE_G),
                            clamp(rank_of(ksq), RANK_2, RANK_7));
-    kingRing[Us] = PseudoAttacks[KING][s] | s;
+    kingRing[Us] = (PseudoAttacks[KING][s] | s) & ~square_bb(ksq);
 
     kingAttackersCount[Them] = popcount(kingRing[Us] & pe->pawn_attacks(Them));
     kingAttacksCount[Them] = kingAttackersWeight[Them] = 0;
