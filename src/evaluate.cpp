cdb852e7aadecff6b703c1aaee80deab2c43fd94
protonspring <mike@whiteley.org>
2019-11-11 22:14:15 -0700

    simplify trapped rook.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -345,11 +345,7 @@ namespace {
 
             // Penalty when trapped by the king, even more if the king cannot castle
             else if (mob <= 3)
-            {
-                File kf = file_of(pos.square<KING>(Us));
-                if ((kf < FILE_E) == (file_of(s) < kf))
                     score -= TrappedRook * (1 + !pos.castling_rights(Us));
-            }
         }
 
         if (Pt == QUEEN)
