455f24d9ecf76e508592cc38fcb4911f1212714f
protonspring <mike@whiteley.org>
2019-11-14 22:07:15 -0700

    tune new backward rule.

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -32,12 +32,18 @@ namespace {
   #define S(mg, eg) make_score(mg, eg)
 
   // Pawn penalties
-  constexpr Score Backward      = S( 9, 24);
   constexpr Score BlockedStorm  = S(82, 82);
   constexpr Score Doubled       = S(11, 56);
   constexpr Score Isolated      = S( 5, 15);
   constexpr Score WeakLever     = S( 0, 56);
   constexpr Score WeakUnopposed = S(13, 27);
+
+  // Based on a count of backward pawns
+  Score Backward[FILE_NB] = {
+    S(6,18),S( 8,20),S(10,22),S(12,24),S(14,26),S(16,28),S(18,30),S(20,32)
+  };
+
+TUNE(Backward);
 
   // Connected pawn bonus
   constexpr int Connected[RANK_NB] = { 0, 7, 8, 12, 29, 48, 86 };
@@ -77,6 +83,7 @@ namespace {
     bool backward, passed, doubled;
     Score score = SCORE_ZERO;
     const Square* pl = pos.squares<PAWN>(Us);
+    int backwardCount = 0;
 
     Bitboard ourPawns   = pos.pieces(  Us, PAWN);
     Bitboard theirPawns = pos.pieces(Them, PAWN);
@@ -143,7 +150,7 @@ namespace {
                      + WeakUnopposed * !opposed;
 
         else if (backward)
-            score -=   Backward
+            score -=   Backward[backwardCount++]
                      + WeakUnopposed * !opposed;
 
         if (!support)
