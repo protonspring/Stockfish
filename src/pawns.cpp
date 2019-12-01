b482af3d1403393e59bebe95055c470f588fb5d0
protonspring <mike@whiteley.org>
2019-12-01 16:58:53 -0700

    9/8

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -41,7 +41,7 @@ namespace {
 
   // Connected pawn bonus
   constexpr int    aBase[RANK_NB] = { 2*0, 2*7, 2*8, 2*12, 2*29, 2*48, 2*86 };
-  constexpr int aPhalanx[RANK_NB] = { 7*0/8, 7*7/8, 7*8/8, 7*12/8, 7*29/8, 7*48/8, 7*86/8 };
+  constexpr int aPhalanx[RANK_NB] = { 9*0/8, 9*7/8, 9*8/8, 9*12/8, 9*29/8, 9*48/8, 9*86/8 };
   constexpr int aOpposed[RANK_NB] = { 0, 7, 8, 12, 29, 48, 86 };
   //constexpr int aSupport[RANK_NB] = {21,21,21, 21, 21, 21, 21 };
 
