87ef0b0e13ed2b877eed74272e063e7e86a9857c
protonspring <mike@whiteley.org>
2019-12-08 09:09:39 -0700

    penalize backward with 2 leverpush.

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -37,6 +37,7 @@ namespace {
   constexpr Score Doubled       = S(11, 56);
   constexpr Score Isolated      = S( 5, 15);
   constexpr Score WeakLever     = S( 0, 56);
+  constexpr Score BadBackward   = S( 0, 14);
   constexpr Score WeakUnopposed = S(13, 27);
 
   // Connected pawn bonus
@@ -144,7 +145,8 @@ namespace {
 
         else if (backward)
             score -=   Backward
-                     + WeakUnopposed * !opposed;
+                     + WeakUnopposed * !opposed
+                     + BadBackward * more_than_one(leverPush);
 
         if (!support)
             score -=   Doubled * doubled
