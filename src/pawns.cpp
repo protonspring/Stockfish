58404281484a3b79ac8b9a22e58f1fd67160e229
protonspring <mike@whiteley.org>
2019-12-09 17:30:20 -0700

    neighbouring pawns only on neighbouring ranks.

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -101,7 +101,7 @@ namespace {
         lever      = theirPawns & PawnAttacks[Us][s];
         leverPush  = theirPawns & PawnAttacks[Us][s + Up];
         doubled    = ourPawns   & (s - Up);
-        neighbours = ourPawns   & adjacent_files_bb(s);
+        neighbours = ourPawns   & (adjacent_files_bb(s) & (rank_bb(s - Up) | rank_bb(s) | rank_bb(s + Up)));
         phalanx    = neighbours & rank_bb(s);
         support    = neighbours & rank_bb(s - Up);
 
