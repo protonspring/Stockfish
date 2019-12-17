99878131013f5252cb07016f6b1e77e5adc23d61
protonspring <mike@whiteley.org>
2019-12-17 14:28:45 -0700

    20

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -467,7 +467,7 @@ namespace {
                  + 148 * popcount(unsafeChecks)
                  +  98 * popcount(pos.blockers_for_king(Us))
                  +  69 * kingAttacksCount[Them]
-                 +  50 * trappedKing
+                 +  20 * trappedKing
                  +   3 * kingFlankAttack * kingFlankAttack / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  - 873 * !pos.count<QUEEN>(Them)
