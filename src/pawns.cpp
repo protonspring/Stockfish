4f597b29302a9b50812c62f7e71d0a4efed9dcc5
protonspring <mike@whiteley.org>
2019-12-17 11:49:04 -0700

    any pawn can block.

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -96,7 +96,7 @@ namespace {
 
         // Flag the pawn
         opposed    = theirPawns & forward_file_bb(Us, s);
-        blocked    = theirPawns & (s + Up);
+        blocked    = (theirPawns | ourPawns) & (s + Up);
         stoppers   = theirPawns & passed_pawn_span(Us, s);
         lever      = theirPawns & PawnAttacks[Us][s];
         leverPush  = theirPawns & PawnAttacks[Us][s + Up];
