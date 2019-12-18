fba80e2da8a4a717958f0ca70954981a40f6bfd9
protonspring <mike@whiteley.org>
2019-12-18 14:44:48 -0700

    tbprobe.cpp

--- src/syzygy/tbprobe.cpp
+++ src/syzygy/tbprobe.cpp
@@ -365,23 +365,23 @@ TBTable<WDL>::TBTable(const std::string& code) : TBTable() {
     Position pos;
 
     key = pos.set(code, WHITE, &st).material_key();
-    pieceCount = pos.count<ALL_PIECES>();
+    pieceCount = pos.count(ALL_PIECES);
     hasPawns = pos.pieces(PAWN);
 
     hasUniquePieces = false;
     for (Color c : { WHITE, BLACK })
         for (PieceType pt = PAWN; pt < KING; ++pt)
-            if (popcount(pos.pieces(c, pt)) == 1)
+            if (pos.count(c, pt) == 1)
                 hasUniquePieces = true;
 
     // Set the leading color. In case both sides have pawns the leading color
     // is the side with less pawns because this leads to better compression.
-    bool c =   !pos.count<PAWN>(BLACK)
-            || (   pos.count<PAWN>(WHITE)
-                && pos.count<PAWN>(BLACK) >= pos.count<PAWN>(WHITE));
+    bool c =   !pos.count(BLACK, PAWN)
+            || (   pos.count(WHITE, PAWN)
+                && pos.count(BLACK, PAWN) >= pos.count(WHITE, PAWN));
 
-    pawnCount[0] = pos.count<PAWN>(c ? WHITE : BLACK);
-    pawnCount[1] = pos.count<PAWN>(c ? BLACK : WHITE);
+    pawnCount[0] = pos.count(c ? WHITE : BLACK, PAWN);
+    pawnCount[1] = pos.count(c ? BLACK : WHITE, PAWN);
 
     key2 = pos.set(code, BLACK, &st).material_key();
 }
@@ -1158,7 +1158,7 @@ void* mapped(TBTable<Type>& e, const Position& pos) {
 template<TBType Type, typename Ret = typename TBTable<Type>::Ret>
 Ret probe_table(const Position& pos, ProbeState* result, WDLScore wdl = WDLDraw) {
 
-    if (pos.count<ALL_PIECES>() == 2) // KvK
+    if (pos.count(ALL_PIECES) == 2) // KvK
         return Ret(WDLDraw);
 
     TBTable<Type>* entry = TBTables.get<Type>(pos.material_key());
