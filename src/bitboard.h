c04951bd56122d60ff49fdb6a1256f5757222857
protonspring <mike@whiteley.org>
2019-12-27 16:46:15 -0700

    try to speed up betweenbb.

--- src/bitboard.h
+++ src/bitboard.h
@@ -199,8 +199,8 @@ inline Bitboard adjacent_files_bb(Square s) {
 /// If the given squares are not on a same file/rank/diagonal, return 0.
 
 inline Bitboard between_bb(Square s1, Square s2) {
-  return LineBB[s1][s2] & ( (AllSquares << (s1 +  (s1 < s2)))
-                           ^(AllSquares << (s2 + !(s1 < s2))));
+  Bitboard b = (AllSquares ^ SQ_H8) >> (64 - abs(s2 - s1));
+  return LineBB[s1][s2] & (b << ((s1 < s2 ? s1 : s2) + 1));
 }
 
 
