f3d7642d5856d41f5f1c5b8df19b3994403c80b7
protonspring <mike@whiteley.org>
2019-12-28 23:35:01 -0700

    /5

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -298,7 +298,7 @@ namespace {
 
                 //More bonus for blocking rooks or queens
                 if (pos.pieces(Them, ROOK, QUEEN) & forward_file_bb(Us, s))
-                    score += Outpost / 3;
+                    score += Outpost / 5;
             }
 
             else if (Pt == KNIGHT && bb & b & ~pos.pieces(Us))
