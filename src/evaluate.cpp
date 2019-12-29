db90e5f04df2fb5ccf59a07e328476b57611ed29
protonspring <mike@whiteley.org>
2019-12-29 09:10:02 -0700

    8,6

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -136,7 +136,7 @@ namespace {
   constexpr Score LongDiagonalBishop = S( 45,  0);
   constexpr Score MinorBehindPawn    = S( 18,  3);
   constexpr Score Outpost            = S( 30, 21);
-  constexpr Score BlockingOutpost    = S(  6,  4);
+  constexpr Score BlockingOutpost    = S(  8,  6);
   constexpr Score PassedFile         = S( 11,  8);
   constexpr Score PawnlessFlank      = S( 17, 95);
   constexpr Score RestrictedPiece    = S(  7,  7);
