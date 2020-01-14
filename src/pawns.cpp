978eb98aac5b39eb1a21d0bcd0db89c17eeeac33
protonspring <mike@whiteley.org>
2020-01-14 13:51:51 -0700

    replace connected array.

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -39,9 +39,6 @@ namespace {
   constexpr Score WeakLever     = S( 0, 56);
   constexpr Score WeakUnopposed = S(13, 27);
 
-  // Connected pawn bonus
-  constexpr int Connected[RANK_NB] = { 0, 7, 8, 12, 29, 48, 86 };
-
   // Strength of pawn shelter for our king by [distance from edge][rank].
   // RANK_1 = 0 is used for files where we have no pawn, or pawn is behind our king.
   constexpr Value ShelterStrength[int(FILE_NB) / 2][RANK_NB] = {
@@ -132,10 +129,10 @@ namespace {
         // Score this pawn
         if (support | phalanx)
         {
-            int v =  Connected[r] * (2 + bool(phalanx) - bool(opposed))
+            int v =  (7 + r * r * r * r / 16) * (2 + bool(phalanx) - bool(opposed))
                    + 21 * popcount(support);
 
-            score += make_score(v, v * (r - 2) / 4);
+            score += make_score(v, v - 15);
         }
 
         else if (!neighbours)
