c0059b6fdc4f6832a368bbc23def24f2818a85e4
protonspring <mike@whiteley.org>
2020-01-28 11:09:28 -0700

    cleanup

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -236,17 +236,7 @@ Score Entry::do_king_safety(const Position& pos) {
   if (pos.can_castle(Us & QUEEN_SIDE))
       shelter = std::max(shelter, evaluate_shelter<Us>(pos, relative_square(Us, SQ_C1)), compare);
 
-  // In endgame we like to bring our king near our closest pawn
-  Bitboard pawns = pos.pieces(Us, PAWN);
-  //int minPawnDist = pawns ? 8 : 0;
-
-  //if (pawns & PseudoAttacks[KING][ksq])
-      //minPawnDist = 1;
-  //else while (pawns)
-      //minPawnDist = std::min(minPawnDist, distance(ksq, pop_lsb(&pawns)));
-
-  //return shelter - make_score(0, 16 * minPawnDist);
-  return shelter + make_score(0,3) * popcount(BigKingBlock[ksq] & pawns);
+  return shelter + make_score(0,3) * popcount(BigKingBlock[ksq] & pos.pieces(Us, PAWN));
 }
 
 // Explicit template instantiation
