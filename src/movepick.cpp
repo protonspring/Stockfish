b98fce1421080535166a3cd16b4ac080276e85fd
protonspring <mike@whiteley.org>
2020-02-02 09:09:55 -0700

    v2

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -33,17 +33,13 @@ namespace {
 
   // partial_insertion_sort() sorts moves in descending order up to and including
   // a given limit. The order of moves smaller than the limit is left unspecified.
-  void partial_insertion_sort(ExtMove* begin, ExtMove* end, int limit) {
+  void partial_sort(ExtMove* begin, ExtMove* end, int limit) {
 
-    for (ExtMove *sortedEnd = begin, *p = begin + 1; p < end; ++p)
-        if (p->value >= limit)
-        {
-            ExtMove tmp = *p, *q;
-            *p = *++sortedEnd;
-            for (q = sortedEnd; q != begin && *(q - 1) < tmp; --q)
-                *q = *(q - 1);
-            *q = tmp;
-        }
+    ExtMove* sortedEnd = begin;
+    std::swap(*sortedEnd, *std::max_element(sortedEnd, end));
+
+    while((sortedEnd->value > limit) && (sortedEnd < end))
+        std::swap(*sortedEnd++, *std::max_element(sortedEnd, end));
   }
 
 } // namespace
@@ -206,7 +202,7 @@ top:
           endMoves = generate<QUIETS>(pos, cur);
 
           score<QUIETS>();
-          partial_insertion_sort(cur, endMoves, -3000 * depth);
+          partial_sort(cur, endMoves, -3000 * depth);
       }
 
       ++stage;
