4f43725c5b84ae0540108976f9b8c789aa7458fe
protonspring <mike@whiteley.org>
2020-03-09 21:16:22 -0600

    15,0

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -337,7 +337,7 @@ namespace {
 
                 // If knight forks two or more more valuable pieces
                 if (more_than_one(PseudoAttacks[KNIGHT][s] & targets))
-                        score += make_score( 0, 15);
+                        score += make_score( 15, 0);
             }
         }
 
