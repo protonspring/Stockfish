3780f0c512f3830d7b43426cf70a4ffd5a82cfbc
protonspring <mike@whiteley.org>
2020-03-12 22:42:24 -0600

    -5, 15

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -191,7 +191,7 @@ Score Entry::evaluate_shelter(const Position& pos, Square ksq) {
   Bitboard ourPawns = b & pos.pieces(Us);
   Bitboard theirPawns = b & pos.pieces(Them);
 
-  Score bonus = make_score(5, 5);
+  Score bonus = make_score(-5, 15);
 
   File center = Utility::clamp(file_of(ksq), FILE_B, FILE_G);
   for (File f = File(center - 1); f <= File(center + 1); ++f)
