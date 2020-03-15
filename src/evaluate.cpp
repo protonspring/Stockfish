cf454456af7f22e8f68ca368b34d644ecbd46c83
protonspring <mike@whiteley.org>
2020-03-15 13:17:44 -0600

    eg

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -299,7 +299,7 @@ namespace {
                 if (Pt == KNIGHT)
                 {
                     int d = 8 - 2 * distance(s, pos.square<KING>(Them));
-                    score += make_score(d,0);
+                    score += make_score(0,d);
                 }
             }
 
