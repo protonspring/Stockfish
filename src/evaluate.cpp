832ea44b810ad4aae91184fb1c97aaad59307d3e
protonspring <mike@whiteley.org>
2020-03-17 11:03:59 -0600

    mg = 19-4d

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -299,7 +299,7 @@ namespace {
                 if (Pt == KNIGHT)
                 {
                     int d = distance(s, pos.square<KING>(Them));
-                    score += make_score(16 - 4 * d,8 - 2 * d);
+                    score += make_score(19 - 4 * d, 0);
                 }
             }
 
