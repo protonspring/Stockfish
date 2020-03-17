37d98c7ee2d98c70d66f21e0755a32a539426c36
protonspring <mike@whiteley.org>
2020-03-17 15:14:53 -0600

    use PseudoAttacks to store SquareBB.

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -27,7 +27,6 @@
 uint8_t PopCnt16[1 << 16];
 uint8_t SquareDistance[SQUARE_NB][SQUARE_NB];
 
-Bitboard SquareBB[SQUARE_NB];
 Bitboard LineBB[SQUARE_NB][SQUARE_NB];
 Bitboard PseudoAttacks[PIECE_TYPE_NB][SQUARE_NB];
 Bitboard PawnAttacks[COLOR_NB][SQUARE_NB];
@@ -72,7 +71,7 @@ void Bitboards::init() {
       PopCnt16[i] = std::bitset<16>(i).count();
 
   for (Square s = SQ_A1; s <= SQ_H8; ++s)
-      SquareBB[s] = (1ULL << s);
+      PseudoAttacks[0][s] = (1ULL << s);
 
   for (Square s1 = SQ_A1; s1 <= SQ_H8; ++s1)
       for (Square s2 = SQ_A1; s2 <= SQ_H8; ++s2)
