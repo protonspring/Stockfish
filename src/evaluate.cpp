a2a0076ac96278ee7b310b619e8666c16da37ded
protonspring <mike@whiteley.org>
2020-04-07 09:48:45 -0600

    expand safe.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -530,7 +530,8 @@ namespace {
     score += RestrictedPiece * popcount(b);
 
     // Protected or unattacked squares
-    safe = ~attackedBy[Them][ALL_PIECES] | attackedBy[Us][ALL_PIECES];
+    safe = ~attackedBy[Them][ALL_PIECES] |
+           (attackedBy[Us][ALL_PIECES] & ~attackedBy2[Them]);
 
     // Bonus for attacking enemy pieces with our relatively safe pawns
     b = pos.pieces(Us, PAWN) & safe;
