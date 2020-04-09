968e3c537c446a339eb68a9e610ae0aa878a64bc
protonspring <mike@whiteley.org>
2020-04-09 17:15:16 -0600

    0,10

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -337,7 +337,7 @@ namespace {
                 //Bonus for valuable pieces within two moves
                 Bitboard targets = pos.pieces(Them, KING, QUEEN) | pos.pieces(Them, ROOK);
                 if (more_than_one(Knight2Move[s] & targets)
-                    score += make_score(10,0);
+                    score += make_score(0,10);
             }
         }
 
