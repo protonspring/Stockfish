378ece37ae2c255bbf6131b11e5689063555104a
protonspring <mike@whiteley.org>
2020-04-10 11:21:25 -0600

    or same file.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -348,7 +348,7 @@ namespace {
             else if (mob <= 3)
             {
                 File kf = file_of(pos.square<KING>(Us));
-                if ((kf < FILE_E) == (file_of(s) < kf))
+                if ((kf <= FILE_E) == (file_of(s) <= kf))
                     score -= TrappedRook * (1 + !pos.castling_rights(Us));
             }
         }
