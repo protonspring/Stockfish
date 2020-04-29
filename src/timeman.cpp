f1894ad83cc7cb990d45273412089c1eb8239db8
protonspring <mike@whiteley.org>
2020-04-29 13:10:18 -0600

    adjust timeLeft instead of minThinkingTime.

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -70,7 +70,8 @@ void TimeManagement::init(Search::LimitsType& limits, Color us, int ply) {
 
   // If there is no time left, use actual game time.
   if (timeLeft == 0)
-      minThinkingTime = std::max<int>(minThinkingTime, limits.time[us] / 24);
+      //minThinkingTime = std::max<int>(minThinkingTime, limits.time[us] / 24);
+      timeLeft = std::max<int>(minThinkingTime, limits.time[us] / 24);
 
   //OPTIMUM TIME
   double scale1 = std::max(2.0, 8.2 * (9.0 - std::log2(ply + 1)));
