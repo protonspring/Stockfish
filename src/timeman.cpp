2656e91019d3e244150caad66bb60432dde0bed7
protonspring <mike@whiteley.org>
2020-05-07 08:48:22 -0600

    some formatting.

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -59,7 +59,7 @@ void TimeManagement::init(Search::LimitsType& limits, Color us, int ply) {
 
   int mtg = limits.movestogo ? std::min(limits.movestogo, 50) : 50;
 
-  // Adjust moveOverhead to support tiny increments (if needed)
+  // Adjust moveOverhead to help with tiny increments (if needed)
   moveOverhead = std::max(10, std::min<int>(limits.inc[us] / 2, moveOverhead));
 
   TimePoint timeLeft =  std::max(TimePoint(0),
@@ -67,23 +67,17 @@ void TimeManagement::init(Search::LimitsType& limits, Color us, int ply) {
 
   timeLeft = slowMover * timeLeft / 100;
 
-///  inc == 0 && movestogo == 0 means: x basetime  [sudden death!]
-///  inc >  0 && movestogo == 0 means: x basetime + z increment
-
+  /// movestogo == 0 means: x basetime (+ z increment)
   if (limits.movestogo == 0)
   {
       scale = 1.0 / std::max(2.0, 8.2 * (9.2 - std::log2(ply + 1)));
       optimumTime = std::min<int>(0.2 * limits.time[us], scale * timeLeft);
-      optimumTime = std::max<int>(minThinkingTime, optimumTime);
 
       scale = std::min(10.0, 5.5 + ply / 26.0);
       maximumTime = std::min<int>(0.8 * limits.time[us] - moveOverhead, scale * optimumTime);
-      maximumTime = std::max<int>(minThinkingTime, maximumTime);
   }
 
-///  inc == 0 && movestogo != 0 means: x moves in y minutes
-///  inc >  0 && movestogo != 0 means: x moves in y minutes + z increment
-
+  /// movestogo != 0 means: x moves in y seconds (+ z increment)
   else
   {
       double mid = (ply - 30.0) / 32.0;
@@ -94,6 +88,9 @@ void TimeManagement::init(Search::LimitsType& limits, Color us, int ply) {
       scale = std::min<double>(5.5, 1.5 + 0.1 * limits.movestogo);
       maximumTime = std::min<int>(limits.time[us] - 2 * limits.movestogo * moveOverhead, scale * optimumTime);
   }
+
+  optimumTime = std::max<int>(minThinkingTime, optimumTime);
+  maximumTime = std::max<int>(minThinkingTime, maximumTime);
     
   if (Options["Ponder"])
       optimumTime += optimumTime / 4;
