1dc95067b67bf7b93545c26638b64daea4226b25
protonspring <mike@whiteley.org>
2020-05-15 08:57:53 -0600

    10

--- src/search.cpp
+++ src/search.cpp
@@ -231,7 +231,7 @@ void MainThread::search() {
                       rootPos.count<KNIGHT>() + rootPos.count<ROOK>() +
                       rootPos.count<PAWN>() + 2;
 
-  int scale = 20.0 * moveCount / pieceCount;
+  int scale = 10.0 * moveCount / pieceCount;
 
   Time.init(Limits, us, scale);
   TT.new_search();
