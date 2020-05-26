a8bdc0c36069d2f7849b3f8a191be2722761613c
protonspring <mike@whiteley.org>
2020-05-25 23:28:04 -0600

    reductions equation.

--- src/search.cpp
+++ src/search.cpp
@@ -194,7 +194,7 @@ namespace {
 void Search::init() {
 
   for (int i = 1; i < MAX_MOVES; ++i)
-      Reductions[i] = int((24.8 + std::log(Threads.size())) * std::log(i));
+      Reductions[i] = int(24.0 + 15.0 * i / (10.0 + std::abs(i / 4.0)));
 }
 
 
