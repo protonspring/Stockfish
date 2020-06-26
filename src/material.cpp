031cffe80f8a57b4663f5ebda142efc17d6e46d7
protonspring <mike@whiteley.org>
2020-06-26 09:17:12 -0600

    36

--- src/material.cpp
+++ src/material.cpp
@@ -166,7 +166,7 @@ Entry* probe(const Position& pos) {
   int imb = 1438 * (whiteBP - blackBP);
 
   //Pawns
-  imb += 40 * (whitePawns * whitePawns - blackPawns * blackPawns);
+  imb += 36 * (whitePawns * whitePawns - blackPawns * blackPawns);
 
   //Knights
   imb += whiteKnights * (-62 * whiteKnights + 255 * whitePawns
