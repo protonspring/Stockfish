a8124e1ebd9353f078c5e1237f0094284e469cb3
protonspring <mike@whiteley.org>
2020-07-06 15:40:31 -0600

    only penalize pawns on same color that are ahead of the bishop.

--- src/position.h
+++ src/position.h
@@ -312,7 +312,7 @@ inline bool Position::advanced_pawn_push(Move m) const {
 }
 
 inline int Position::pawns_on_same_color_squares(Color c, Square s) const {
-  return popcount(pieces(c, PAWN) & ((DarkSquares & s) ? DarkSquares : ~DarkSquares));
+  return popcount(forward_ranks_bb(c, s) & pieces(c, PAWN) & ((DarkSquares & s) ? DarkSquares : ~DarkSquares));
 }
 
 inline Key Position::key() const {
