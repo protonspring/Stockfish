6ffa82314d9d02720273ef7815ea16babc212b1b
protonspring <mike@whiteley.org>
2020-08-10 15:44:22 -0600

    4k

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -945,7 +945,7 @@ Value Eval::evaluate(const Position& pos) {
   {
       Value v = eg_value(pos.psq_score());
       // Take NNUE eval only on balanced positions
-      if (abs(v) < NNUEThreshold)
+      if (((abs(v) < NNUEThreshold) && (pos.non_pawn_material() > 4000)))
          return NNUE::evaluate(pos) + Tempo;
   }
   return Evaluation<NO_TRACE>(pos).value();
